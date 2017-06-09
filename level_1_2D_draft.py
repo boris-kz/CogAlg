ed89c16473c2011f554e5f8e4f69fae8daab6918
boris-kz <boris.kz@gmail.com>
2017-06-08 23:08:01 -0400

    misc

--- level_1_2D_draft.py
+++ level_1_2D_draft.py
@@ -6,7 +6,7 @@ Level 1 with 2D gradient: modified combination of core algorithm levels 1 and 2
 
 Initial 2D comparison forms lateral and vertical derivatives: 2 matches and 2 differences per pixel. Both lateral and vertical comparison is
 performed on the same level because average lateral match ~ average vertical match. These derivatives form quadrant gradients:
-average of rightward and downward match or difference per pixel (they are equally representative samples of quadrant).
+average of rightward and downward match or difference per pixel (these are equally representative samples of quadrant).
 Quadrant gradient is a minimal unit of 2D gradient, so 2D pattern (blob) is defined by matching sign of 
 quadrant gradient of value for vP, or quadrant gradient of difference for dP.
 
@@ -17,60 +17,59 @@ def comp(p, pri_p, _p, _d, _m, fd, fv, dy, my, _ip_,  # input variables
          pri_sd, Id, Dd, Vd, d_, owd, dalt_,  # variables of dP
          x, _x, X, y, Y, r, A, vP_, dP_, _vP_, _dP_): # parameters and output
 
-    # last "_" denotes array vs. element, first "_" denotes array, pattern, or variable from higher line
+    # last "_" denotes array vs. element, first "_" denotes template: higher-line array, pattern, or variable
 
-    d = p - pri_p    # difference between laterally consecutive pixels
+    d = p - pri_p    # difference between laterally consecutive pixels, also -> D for vertical comp?
     if y > 0: dy = p - _p  # difference between vertically consecutive pixels, else dy = 0
 
-    dq = _d + dy     # quadrant gradient of difference per prior-line pixel _p
+    dq = _d + dy     # quadrant gradient of difference per prior-line pixel _p,
     fd += dq         # all shorter + current- range dq s within extended quadrant?
 
-    m = min(p, pri_p)  # match between laterally consecutive pixels
+    m = min(p, pri_p)  # match between laterally consecutive pixels, -> M for vertical comp, vq sum per P2?
     if y > 0: my = min(p, _p)  # match between vertically consecutive pixels, else my = 0
 
-    vq = _m + my - A # quadrant gradient of predictive value (relative match) per prior-line _p
+    vq = _m + my - A  # quadrant gradient of predictive value (relative match) per prior-line _p
     fv += vq         # all shorter + current- range vq s within extended quadrant
 
 
     # formation of 1D value pattern vP: horizontal span of same-sign vq s:
 
     s = 1 if vq > 0 else 0  # s: positive sign of vq
-    if x > r+2 and (s != pri_s or x == X-1):  # vP is terminated if vq sign miss or line ends
+    if x > r+2 and (s != pri_s or x == X-1):  # if vq sign miss or line ends, vP is terminated
 
-        vP = pri_s, I, D, V, p_, alt_  # no default div: redundancy eval per P2 on Le2?
-        vP_.append(vP)  # or vP_ is within vP2, after comp_vP:
+        vP = pri_s, I, D, V, p_, alt_
+        if y > 1: comp_P(vP, _vP_, x, _x, y, Y)  # end: vP_.append(vP), separate comp_vP and comp_dP?
 
-        if y > 1: comp_P(vP, _vP_, x, _x, y, Y)  # called from comp() if P term, separate comp_vP and comp_dP?
+        alt = len(vP_), ow  # len(vP_) is index of current vP
+        dalt_.append(alt)  # index and ow of terminated vP is buffered at current dP
 
-        alt = len(vP_), ow  # len(P_) is an index of last overlapping vP
-        dalt_.append(alt)  # addresses of overlapping vPs and ow are buffered at current dP
         I, D, V, ow, owd, p_, alt_ = 0, 0, 0, 0, 0, [], []  # initialization of new vP and ow
 
     pri_s = s   # vP (representing span of same-sign vq s) is incremented:
     ow += 1     # overlap to current dP
     I += pri_p  # ps summed within P width
     D += dq     # fds summed within P width
-    V += fv     # fvs summed within P width
+    V += fv     # fvs summed within P width, generic name for summed vars is S:
 
-    pri = pri_p, fd, fv  # same-line prior tuple,
-    p_.append(pri)  # buffered within P for selective inc_rng comp
+    # lateral S for vertical comp -> potential adjustment by orientation? not for ow: relative to w?
+    # vertical S for complementary adjustment and P2 gradient, no dq or vq sum: redundant?
 
-    high = p, d, m  # new higher-line tuple, m sum \ P, v sum \ P2: compressed?
-    _ip_.append(high)  # buffered for next-line comp
-
+    pri = pri_p, fd, fv  # same-line prior tuple, buffered for selective inc_rng comp
+    p_.append(pri)
+    high = p, d, m  # next-higher-line tuple, buffered for spec of next-line vertical D, M comp?
+    _ip_.append(high)
 
     # formation of difference pattern dP: horizontal span of same-sign dq s:
 
     sd = 1 if d > 0 else 0  # sd: positive sign of d;
-    if x > r+2 and (sd != pri_sd or x == X-1):  # if derived pri_sd miss, dP is terminated
+    if x > r+2 and (sd != pri_sd or x == X-1):  # if derived pri_sd miss or line ends, dP is terminated
 
         dP = pri_sd, Id, Dd, Vd, d_, dalt_
-        dP_.append(dP)  # output of dP
+        if y > 1: comp_P(dP, _dP_, x, _x, y, Y)  # end: dP_.append(dP), separate comp_vP and comp_dP?
 
-        if y > 1: comp_P(dP, _dP_, x, _x, y, Y)  # called from comp() if P term, separate comp_vP and comp_dP?
+        alt = len(dP_), owd  # len(dP_) is index of current dP
+        alt_.append(alt)  # index and owd of terminated dP is buffered at current vP
 
-        alt = len(dP_), owd  # len(P_) is an address of last overlapping dP
-        alt_.append(alt)  # addresses and owd of overlapping dPs are buffered at current vP
         Id, Dd, Vd, ow, owd, d_, dalt_ = 0, 0, 0, 0, 0, [], []  # initialization of new dP and ow
 
     pri_sd = sd  # dP (representing span of same-sign dq s) is incremented:
@@ -78,7 +77,7 @@ def comp(p, pri_p, _p, _d, _m, fd, fv, dy, my, _ip_,  # input variables
     Id += pri_p  # ps summed within P width
     Dd += fd     # fds summed within P width
     Vd += fv     # fvs summed within P width
-    d_.append(fd)  # prior fds buffered in P for select inc_der, no associated derivatives, no lateral high?
+    d_.append(fd)  # same fds as in p_, no associated derivatives, separate for spec of vert dP D comp?
 
 
     return _x, _ip_, vP_, dP_, _vP_, _dP_, \
@@ -105,12 +104,12 @@ def comp_P(P, _P_, x, _x, y, Y):  # called from comp() if P term, _x is from las
     s, I, D, M, r, e_, alt_ = P  # M vs. V: no lateral eval, V = M - 2a * W?
     w = len(e_); ix = x - w  # initial x coordinate?
 
-    while x > _x:  # horizontal overlap between P and next _P, no redundancy eval till CP2 term?
+    while x > _x:  # horizontal overlap between P and next _P, forks are not redundant, P2: mult. uni-forks only?
 
         _P = _P_.pop(); n += 1  # to sync with cfork_
         _s, _ix, _x, _w, _I, _D, _M, _r, _e_, _alt_, _fork_, _root_ = _P  # old _fork_, _root_ for trunk tracing?
 
-        if s == _s: # !eval, ~dP? selected at y_buff_.append(P): - cost of fork for weaker matches?
+        if s == _s: # !eval, ~dP? - cost of fork unless unique, at y_buff_.append(P), -> P2 if only one recorded?
 
             W +=_w; I2 +=_I; D2 +=_D; M2 +=_M; P_.append(_P)
             root_.append(len(_P_))  # index of connected _P within _P_
@@ -119,8 +118,11 @@ def comp_P(P, _P_, x, _x, y, Y):  # called from comp() if P term, _x is from las
             dx = x - w/2 - _x - _w/2  # mx = mean_dx - dx, signed, or unsigned overlap?
             dw = w -_w; mw = min(w, _w)  # orientation if difference decr / match incr for min.1D Ps over max.2D:
 
-            comp(dx), comp(dw, ddx) # at dxP term?
-            if match(dw, ddx) > a: _w *= cos(ddx); comp(w, _w)  # proj w*cos match, if same-sign dw, ddx, not |.|
+            # comp(dx), comp(dw, ddx) at dxP term?
+            # if match(dw, ddx) > a: _w *= cos(ddx); comp(w, _w)  # proj w*cos match, if same-sign dw, ddx, not |.|
+
+            # comp of separate lateral D and M
+            # default div and overlap eval per P2? not CP2: sparse coverage?
 
         ''' 
         if mx+mw > a: # conditional input vars norm and comp, also at P2 term: rotation if match (-DS, Ddx),  div_comp if rw?  
