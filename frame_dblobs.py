fc22c0b4a29ff7ececf06bfb822731a8a538f6c4
Boris Kazachenko <boris.kz@gmail.com>
2018-12-14 20:16:44 -0500

    last update by khanh93vn, with minor edits

--- frame_dblobs.py
+++ frame_dblobs.py
@@ -47,23 +47,31 @@ from collections import deque
 # -rebuild_blobs()
 # ***********************************************************************************************************************
 
-def rebuild_blobs( frame ):
+def rebuild_blobs(frame):
     " Rebuilt data of blobs into an image "
-    blob_image = numpy.array([[255] * X] * Y)
+    blob_image = numpy.array([[[0] * 4] * X] * Y)
 
     for index, blob in enumerate(frame[9]):  # Iterate through blobs
-        if not blob[0][0]:  # Choose negative dblobs
-            for seg in blob[1]:  # Iterate through segments
-                y = seg[7] - len(seg[5]) + 1
-                for (P, dx) in seg[5]:
+        for seg in blob[1]:  # Iterate through segments
+            y = seg[7] - len(seg[5]) + 1
+            for (P, dx) in seg[5]:
+                if P[0]:
                     x = P[1]
-                    for (p, d, dy, m, my) in P[8]:
-                        blob_image[y, x] = 0
+                    for i in range(P[2]):
+                        blob_image[y, x, : 3] = [255, 255, 255]
                         x += 1
-                    y += 1
+                else:
+                    for dP in P[8]:
+                        x = dP[1]
+                        for i in range(dP[2]):
+                            blob_image[y, x, : 3] = [127, 127, 127] if dP[0] else [63, 63, 63]
+                            x += 1
+
+                y += 1
 
     return blob_image
     # ---------- rebuild_blobs() end ------------------------------------------------------------------------------------
+
 
 # ************ UTILITY FUNCTIONS END ************************************************************************************
 
@@ -89,7 +97,6 @@ def lateral_comp(pixel_):
     for x, p in enumerate(pixel_):  # pixel p is compared to rng of prior pixels within horizontal line, summing d and m per prior pixel
         back_fd, back_fm = 0, 0  # fuzzy derivatives from rng of backward comps per pri_p
         for index, (pri_p, fd, fm) in enumerate(rng_ders1_):
-
             d = p - pri_p
             m = ave - abs(d)
             fd += d  # bilateral fuzzy d: running sum of differences between pixel and all prior and subsequent pixels within rng
@@ -103,14 +110,13 @@ def lateral_comp(pixel_):
                 ders1_.append((pri_p, fd, fm))  # completed bilateral tuple is transferred from rng_ders_ to ders_
 
         rng_ders1_.appendleft((p, back_fd, back_fm))  # new tuple with initialized d and m, maxlen displaces completed tuple
-    ders1_ += reversed(rng_ders1_)  # tuples of last rng in line (incomplete, in reverse order), or discarded?
+    # last incomplete rng_ders1_ in line are discarded, vs. ders1_ += reversed(rng_ders1_)
     return ders1_
     # ---------- lateral_comp() end -------------------------------------------------------------------------------------
 
 
-def vertical_comp(ders1_, ders2__, _dP_, dframe):
-    " Comparison between bilateral rng of vertically consecutive pixels, forming ders2: \
-    tuple of pixel + its 2D derivatives "
+def vertical_comp(ders1_, ders2__, _dP_, dframe ):
+    " Comparison to bilateral rng of vertically consecutive pixels, forming ders2: pixel + lateral and vertical derivatives"
 
     dP = 0, rng, 0, 0, 0, 0, 0, 0, []  # lateral difference pattern = pri_s, x0, L, I, D, Dy, V, Vy, ders2_
     dP_ = deque()  # line y - 1 + rng*2
@@ -143,13 +149,6 @@ def vertical_comp(ders1_, ders2__, _dP_, dframe):
         new_ders2__.append(ders2_)  # 2D array of vertically-incomplete 2D tuples, converted to ders2__, for next-line vertical comp
         x += 1
 
-    if y > min_coord + ini_y:  # not-terminated P at the end of each line is buffered or scanned:
-
-        if y == rng * 2 + ini_y:  # _P_ initialization by first line of Ps, empty until vertical_comp returns P_
-            dP_.append([dP, 0, [], x - 1])  # empty _fork_ in the first line of hPs, x-1: delayed P displacement
-        else:
-            dP_, dbuff_, _dP_, dframe = scan_P_(x - 1, dP, dP_, dbuff_, _dP_, dframe)  # scans higher-line Ps for contiguity
-
     return new_ders2__, dP_, dframe
     # ---------- vertical_comp() end ------------------------------------------------------------------------------------
 
@@ -159,10 +158,9 @@ def form_P(ders, x, P, P_, buff_, hP_, frame):
 
     p, d, dy, m, my = ders  # 2D tuple of derivatives per pixel, "y" denotes vertical vs. lateral derivatives
     s = 1 if d > 0 else 0  # core = 0 is negative: no selection?
+    pri_s, x0, L, I, D, Dy, M, My, ders_ = P
 
-    if s == P[0] or x == rng:  # s == pri_s or initialized: P is continued, else terminated:
-        pri_s, x0, L, I, D, Dy, M, My, ders_ = P
-    else:
+    if not ( s == pri_s or x == rng ) or x == X - rng:  # P is terminated
         if y == rng * 2 + ini_y:  # 1st line: form_P converts P to initialized hP, forming initial P_ -> hP_
             P_.append([P, 0, [], x - 1])  # P, roots, _fork_, x
         else:
@@ -194,7 +192,7 @@ def scan_P_(x, P, P_, _buff_, hP_, frame):
         if _buff_:
             hP = _buff_.popleft()  # hP buffered in prior scan_P_
         elif hP_:
-            hP, frame = form_segment( hP_.popleft(), frame )
+            hP, frame = form_segment(hP_.popleft(), frame)
         else:
             break  # higher line ends, all hPs are converted to segments
 
@@ -219,13 +217,15 @@ def scan_P_(x, P, P_, _buff_, hP_, frame):
     # ---------- scan_P_() end ------------------------------------------------------------------------------------------
 
 
-def form_segment( hP, frame ):
+def form_segment(hP, frame):
     " Convert hP into new segment or add it to higher-line segment, merge blobs "
     _P, roots = hP[:2]
+    ave_x = (_P[2] - 1) // 2  # extra-x L = L-1 (1x in L)
 
     if y == rng * 2 + 1 + ini_y:  # scan_P_ of the 1st line converts each hP into blob segment:
         hP[0] = list(_P[2:8])
         hP += 0, [(_P, 0)], [_P[0], 0, 0, 0, 0, 0, 0, _P[1], hP[3], y - rng - 1, [hP], 1, 0]  # initialize blob, min_y = current y
+        hP[3] = ave_x
     else:
         if len(hP[2]) == 1 and hP[2][0][1] == 1:  # hP has one fork: hP[2][0], and that fork has one root: hP
             # hP is merged into higher-line blob segment (Pars, roots, _fork_, ave_x, Dx, Py_, blob) at hP[2][0]:
@@ -233,9 +233,8 @@ def form_segment( hP, frame ):
             Ls, Is, Ds, Dys, Ms, Mys = hP[2][0][0]
             hP[2][0][0] = [Ls + L, Is + I, Ds + D, Dys + Dy, Ms + M, Mys + My]  # seg parameters
             hP[2][0][1] = roots
-            ave_x = (_P[2] - 1) // 2  # extra-x L = L-1 (1x in L)
-            hP[2][0][3] = ave_x
             dx = ave_x - hP[2][0][3]
+            hP[2][0][3] = ave_x
             hP[2][0][4] += dx  # xD for seg normalization and orientation, or += |dx| for curved yL?
             hP[2][0][5].append((_P, dx))  # Py_: vertical buffer of Ps merged into seg
             blob = hP[2][0][6]
@@ -245,15 +244,18 @@ def form_segment( hP, frame ):
 
         elif not hP[2]:  # seg is initialized with initialized blob
             hP[0] = list(_P[2:8])  # seg parameters
-            hP += 0, [(_P, 0)], [_P[0], 0, 0, 0, 0, 0, 0, _P[1], hP[3], y - rng - 1 , [hP], 1, 0]
+            hP += 0, [(_P, 0)], [_P[0], 0, 0, 0, 0, 0, 0, _P[1], hP[3], y - rng - 1, [hP], 1, 0]
             # last var is blob: s, L, I, D, Dy, M, My, min_x, max_x, min_y, root_, remaining_roots, xD
+            hP[3] = ave_x
 
         else:  # if >1 forks, or 1 fork that has >1 roots:
-            hP[0] = list(_P[2:8]); hP += 0, [(_P, 0)], hP[2][0][6]  # seg is initialized with fork's blob
+            hP[0] = list(_P[2:8])
+            hP += 0, [(_P, 0)], hP[2][0][6]  # seg is initialized with fork's blob
             blob = hP[6]
             blob[10].append(hP)  # segment is buffered into root_
             blob[7] = min(_P[1], blob[7])  # min_x
             blob[8] = max(hP[3], blob[8])  # max_x
+            hP[3] = ave_x
 
             if len(hP[2]) > 1:  # merge blobs of all forks
                 if hP[2][0][1] == 1:
@@ -277,7 +279,7 @@ def form_segment( hP, frame ):
                         blob[12] += fork[6][12]
                         for seg in fork[6][10]:
                             if not seg is fork:
-                                seg[6] = blob         # blobs in other forks are references to blob in the first fork
+                                seg[6] = blob  # blobs in other forks are references to blob in the first fork
                                 blob[10].append(seg)  # buffer of merged root segments
                         fork[6] = blob
                         blob[10].append(fork)
@@ -297,7 +299,7 @@ def form_blob(term_seg, frame, y_carry=0):
     blob[5] += M
     blob[6] += My
     blob[11] += roots - 1  # reference to term_seg is already in blob[9]
-    blob[12] += xD   # ave_x angle, to evaluate blob for re-orientation
+    blob[12] += xD  # ave_x angle, to evaluate blob for re-orientation
     term_seg.append(y - rng - 1 - y_carry)  # y_carry: elevation of term_seg over current hP
 
     if not blob[11]:  # if remaining_roots == 0: blob is terminated and packed in frame
@@ -322,7 +324,8 @@ def image_to_blobs(image):
 
     _P_ = deque()  # higher-line same- d-, m-, dy-, my- sign 1D patterns
     frame = [0, 0, 0, 0, 0, 0, 0, 0, 0, []]  # L, I, D, Dy, M, My, xD, b_width, b_height, blob_
-    global y; y = ini_y  # initial line
+    global y
+    y = ini_y  # initial line
     ders2__ = []  # horizontal line of vertical buffers: 2D array of 2D tuples, deque for speed?
     pixel_ = image[ini_y, :]  # first line of pixels at y == 0
     ders1_ = lateral_comp(pixel_)  # after partial comp, while x < rng?
@@ -344,13 +347,13 @@ def image_to_blobs(image):
     y = Y
     hP_ = _P_
     while hP_:
-        hP, frame = form_segment( hP_.popleft(), frame )
+        hP, frame = form_segment(hP_.popleft(), frame)
         frame = form_blob(hP, frame)
     return frame  # frame of 2D patterns, to be outputted to level 2
     # ---------- image_to_blobs() end -----------------------------------------------------------------------------------
 
-# ************ MAIN FUNCTIONS END ***************************************************************************************
 
+# ************ MAIN FUNCTIONS END ***************************************************************************************
 
 
 # ************ PROGRAM BODY *********************************************************************************************
@@ -359,18 +362,18 @@ def image_to_blobs(image):
 # eventually updated by higher-level feedback, initialized here as constants:
 
 rng = 2  # number of pixels compared to each pixel in four directions
-ave = 15  # |d| value that coincides with average match: mP filter
+ave = 31  # |d| value that coincides with average match: mP filter
 ave_rate = 0.25  # not used; match rate: ave_match_between_ds / ave_match_between_ps, init at 1/4: I / M (~2) * I / D (~2)
 ini_y = 0  # not used
 
 # Load inputs --------------------------------------------------------------------
-image = misc.face(gray=True)  # read image as 2d-array of pixels (gray scale):
-image = image.astype(int)
+# image = misc.face(gray=True)  # read image as 2d-array of pixels (gray scale):
+# image = image.astype(int)
 # or:
-# argument_parser = argparse.ArgumentParser()
-# argument_parser.add_argument('-i', '--image', help='path to image file', default='./images/test_blobs.jpg')
-# arguments = vars(argument_parser.parse_args())
-# image = cv2.imread(arguments['image'], 0).astype(int)
+argument_parser = argparse.ArgumentParser()
+argument_parser.add_argument('-i', '--image', help='path to image file', default='./images/raccoon_eye.jpg')
+arguments = vars(argument_parser.parse_args())
+image = cv2.imread(arguments['image'], 0).astype(int)
 
 Y, X = image.shape  # image height and width
 
@@ -381,7 +384,7 @@ end_time = time() - start_time
 print(end_time)
 
 # Rebuild blob -------------------------------------------------------------------
-cv2.imwrite('./images/blobs.jpg', rebuild_blobs( frame_of_blobs ))
+cv2.imwrite('./images/blobs.jpg', rebuild_blobs(frame_of_blobs))
 
 # Check for redundant segments  --------------------------------------------------
 print 'Searching for redundant segments...\n'
