2966a9737ed29c17b8e7ae5aea84bd2a61c5ed25
Boris Kazachenko <boris.kz@gmail.com>
2018-12-14 19:54:28 -0500

    Update line_x_POC.py

--- line_x_POC.py
+++ line_x_POC.py
@@ -26,10 +26,10 @@ def form_pattern(dderived, P, P_, pri_p, d, m, rdn, rng, x, X):  # accumulation,
     s = 1 if m >= 0 else 0  # sign, 0 is positive?   form_P-> pri mP ( sub_dP_, no type:
     pri_s, L, I, D, M, r, e_ = P  # depth of elements in e_ = r: depth of prior comp recursion within P
 
-    if not (x > rng * 2 and s != pri_s) or x == X-1:  # m sign change, mP is terminated and evaluated for recursive comp
+    if ( x > rng * 2 and s == pri_s ) or x == X-1:  # m sign change, mP is terminated and evaluated for recursive comp
         if pri_s:  # forms sub_mP_ within positive mP
 
-            if L > rng + 3 and M < ave_M * rdn:  # comp range increase within e_:
+            if L > rng + 3 and M * rdn < ave_M:  # comp range increase within e_:
                 r = 1                    # rdn: redundancy, incremented per comp recursion
                 rng += 1
                 sub_mP_= []; sub_mP = 0,0,0,0,0,0,[]  # pri_s, L, I, D, M, r, e_;  no Alt: M is defined through abs(d)
@@ -51,41 +51,36 @@ def form_pattern(dderived, P, P_, pri_p, d, m, rdn, rng, x, X):  # accumulation,
                 e_= sub_mP_  # ders replaced with sub_mPs: spans of pixels that form same-sign m
 
         else:   # forms dP_ within negative mP
-            dP_ = []; dP = 0, 0, 0, 0, 0, 0, []  # pri_s, L, I, D, M, r, e_
-            pri_sd = -1
-            for i in range(P[2]):
-                ip, id, im = P[6][i]
+            dP_ = []; dP = int(e_[0][1] > 0), 0, 0, 0, 0, 0, []  # pri_s, L, I, D, M, r, e_
+            e_.append( ( 0, 0, 0 ) )
+
+            for i in range(L + 1):
+                ip, id, im = e_[i]
                 sd = 1 if id > 0 else 0
-                if ( pri_sd == sd or i == 1 ) and i < P[2] - 1:
-                    sd, Ld, Id, Dd, Md, sr, se_ = dP
-                else:
-                    if L > rng + 3 and M < ave_M * rdn:  # comp range increase within e_:
-                        r = 1                    # rdn: redundancy, incremented per comp recursion
-                        rng += 1
-                        sub_mP_= []; sub_mP = 0,0,0,0,0,0,[]  # pri_s, L, I, D, M, r, e_;  no Alt: M is defined through abs(d)
+                pri_sd, Ld, Id, Dd, Md, rd, ed_ = dP
 
-                        for i in range(rng, L):  # comp between rng-distant pixels, also bilateral, if L > rng * 2?
-                            ip, fd, fm = se_[i]
-                            _ip, _fd, _fm = e_[i - rng]
-                            ed = ip - _ip  # ed: element d, em: element m:
-                            if dderived:
-                                em = min(ip, _ip) - ave_m  # magnitude of vars derived from d corresponds to predictive value, thus direct match
-                            else:
-                                em = ave_d - abs(ed)  # magnitude of brightness has low correlation with stability, thus match is defined through d
-                            fd += ed
-                            fm += em  # accumulates difference and match between ip and all prior ips in extended rng
-                            se_[i] = (ip, fd, fm)
-                            _fd += ed  # accumulates difference and match between ip and all prior and subsequent ips in extended rng
-                            _fm += em
-                            sub_mP, sub_mP_ = form_pattern(dderived, sub_mP, sub_mP_, _ip, _fd, _fm, rdn+1, rng, i, L)
-                        se_ = sub_mP_  # ders replaced with sub_mPs: spans of pixels that form same-sign m
+                if ( pri_sd != sd and not i == 0 ) or i == L:
+                    if Ld > rng + 3 and Dd > ave_M * rdn:   # comp range increase within e_:
+                        rd = 1                              # rdn: redundancy, incremented per comp recursion
+                        mdP_= []; mdP = 0,0,0,0,0,0,[]  # pri_s, L, I, D, M, r, e_;  no Alt: M is defined through abs(d)
+                        fdd, fmd = 0, 0
+                        for j in range(1, Ld):  # bilateral comp between consecutive dj s
+                            dj = ed_[j][1]
+                            _dj = ed_[j-1][1]
+                            dd = dj - _dj
+                            md = min(dj, _dj) - ave_m  # magnitude of vars derived from d corresponds to predictive value, thus direct match
+                            fdd += dd  # bilateral difference and match between ds
+                            fmd += md
+                            mdP, mdP_ = form_pattern(1, mdP, mdP_, _dj, fdd, fmd, rdn+1, rng, i, L)
+                            fdd = dd
+                            fmd = md
+                        ed_ = mdP_  # ders are replaced with mdPs: spans of pixels that form same-sign md
 
-                    dP_.append(dP)
-                    sd, Ld, Id, Dd, Md, sr, se_ = 0, 0, 0, 0, 0, 0, []
+                    dP_.append( ( pri_sd, Ld, Id, Dd, Md, rd, ed_ ) )
+                    sd, Ld, Id, Dd, Md, dr, de_ = 0, 0, 0, 0, 0, 0, []
 
-                Ld += 1; Id += ip; Dd += id; Md += im; se_.append( ( ip, id, im ) )
-                dP = sd, i, Ld, Id, Dd, Md, r, se_
-                pri_sd = sd
+                Ld += 1; Id += ip; Dd += id; Md += im; ed_.append( ( ip, id, im ) )
+                dP = sd, Ld, Id, Dd, Md, rd, ed_
             e_= dP_
 
         P_.append((pri_s, L, I, D, M, r, e_))  # terminated P output to second level; x == X-1 doesn't always terminate?
@@ -129,13 +124,13 @@ def cross_comp(frame_of_pixels_):  # postfix '_' denotes array name, vs. identic
                 elif x > min_rng * 2 - 1:
                     P, P_ = form_pattern(0, P, P_, pri_p, fd, fm, 1, min_rng, x, X)  # forms mPs: spans of pixels with same-sign m
 
-                    ders_.appendleft((p, back_fd, back_fm))  # new tuple with initialized d and m, maxlen displaces completed tuple from rng_t_
+            ders_.appendleft((p, back_fd, back_fm))  # new tuple with initialized d and m, maxlen displaces completed tuple from rng_t_
         frame_of_patterns_ += [P_]  # line of patterns is added to frame of patterns, last incomplete ders are discarded
     return frame_of_patterns_  # frame of patterns is output to level 2
 
 
 argument_parser = argparse.ArgumentParser()
-argument_parser.add_argument('-i', '--image', help='path to image file', default='./images/raccoon.jpg')
+argument_parser.add_argument('-i', '--image', help='path to image file', default='./images/raccoon_eye.jpg')
 arguments = vars(argument_parser.parse_args())
 image = cv2.imread(arguments['image'], 0).astype(int)
 
@@ -150,7 +145,7 @@ ave_m = 10  # min dm for positive dmP
 ave_d = 20  # |difference| between pixels that coincides with average value of mP - redundancy to overlapping dPs
 ave_M = 127  # min M for initial incremental-range comparison(t_)
 ave_D = 127  # min |D| for initial incremental-derivation comparison(d_)
-ini_y = 400
+ini_y = 0
 min_rng = 2  # fuzzy pixel comparison range, initialized here but eventually adjusted by higher-level feedback
 Y, X = image.shape  # Y: frame height, X: frame width
 
@@ -158,3 +153,4 @@ start_time = time()
 frame_of_patterns_ = cross_comp(image)
 end_time = time() - start_time
 print(end_time)
+
