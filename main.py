8fd4553e831c302ec55317b5622c4e96936b1454
khanh93vn <khanh93vn@gmail.com>
2019-01-15 22:24:52 +0700

    bugfixes and edits in frame_blobs.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from time import time
-from CAclasses import frame
-from CAmiscs import drawFrame
-
-start_time = time()
-frame = frame('./images/raccoon_eye.jpg')
-drawFrame('./images/raccoon_eye_blobs.bmp', frame)
-print time() - start_time
