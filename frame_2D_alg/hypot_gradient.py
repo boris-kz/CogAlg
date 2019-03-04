c6e4e68029fd9d3ab148c13f9ba5f77609bb1b12
Boris Kazachenko <boris.kz@gmail.com>
2019-03-04 10:33:08 -0500

    please check sub_blob termination and sub_blob_ append

--- frame_2D_alg/hypot_gradient.py
+++ frame_2D_alg/hypot_gradient.py
@@ -1,36 +1,38 @@
 import numpy as np
 from collections import deque
-# Filters ------------------------------------------------------------------------
 from misc import get_filters
-get_filters(globals())          # imports all filters at once
+from frame_2D_alg import generic
+get_filters(globals())   # imports all filters at once
 # --------------------------------------------------------------------------------
 
-def hypot_gradient(blob):  # compute and compare angle, define ablobs, accumulate a, da, sda in all reps within gblob
+def hypot_gradient(blob):  # compute hypot_g, define sub_blobs by incremented filters, extend blob params with hypot flag?
     ''' same functionality as image_to_blobs() in frame_blobs.py'''
 
     global height, width
     height, width = blob.map.shape
-
-    sub_blob = [0, 0, 0, 0, []]  # initialize sub_blob
+    sub_blob = [0, 0, 0, 0, []]  # explain elements?
+    sub_blob_ = []
     seg_ = deque()
     dert__ = blob.dert__
 
-    recalc_g(sub_blob, blob.dert__)
+    dert__.mask = ~blob.map
+    dert__[:, :, 3] = np.hypot(dert__[:, :, 1], dert__[:, :, 2]) - ave * 2  # ave increased to reflect angle_blobs + eval cost
+    sub_blob.append(dert__)  # ref to external map, redundant to dert_ in Ps
 
     for y in range(1, height - 1):
-        P_ = generic_functions.form_P_(y, sub_blob)  # horizontal clustering
-        P_ = generic_functions.scan_P_(P_, seg_, sub_blob)
-        seg_ = generic_functions.form_seg_(P_, sub_blob)
+        P_ = generic.form_P_(y, sub_blob)  # horizontal clustering
+        P_ = generic.scan_P_(P_, seg_, sub_blob)
+        seg_ = generic.form_seg_(P_, sub_blob)
+        
+        if sub_blob.open_segs == 0:  # please check, where is open_segs?  
+            sub_blob_.append(sub_blob)  # sub_blob is terminated
 
-    while seg_: generic_functions.form_blob(seg_.popleft(), sub_blob)
-    blob.sub_blob_.append(sub_blob)
+    y = height - 1   # sub_blob ends, merge segs of last line into their blobs:
+    while seg_: 
+        generic.form_blob(y, seg_.popleft(), sub_blob)
+        if sub_blob.open_segs == 0:  # please check, where is open_segs?  
+            sub_blob_.append(sub_blob)  # sub_blob is terminated
 
-    return sub_blob
+    blob.seg_ = sub_blob_  # replace top-level element_ with sub_blob_, appended in form_blob?
+    return blob
     # ---------- hypot_gradient() end -----------------------------------------------------------------------------------
-
-def recalc_g(sub_blob, dert__):
-
-    dert__.mask = ~blob.map
-    dert__[:, :, 3] = np.hypot(dert__[:, :, 1], dert__[:, :, 2]) - ave
-    sub_blob.append(dert__)
-    # ---------- recalc_g() end -----------------------------------------------------------------------------------------
