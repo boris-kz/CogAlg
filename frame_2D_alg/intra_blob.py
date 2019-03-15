3ab487507284cfc7c54eae02920d50260c052d0c
Boris Kazachenko <boris.kz@gmail.com>
2019-03-15 19:11:48 -0400

    added val_angle to select for inc_deriv(ga), removed direct val_der_a and val_rng_a

--- frame_2D_alg/intra_blob.py
+++ frame_2D_alg/intra_blob.py
@@ -4,24 +4,20 @@ from comp_angle import comp_angle
 from comp_inc_deriv import inc_deriv
 from comp_inc_range import inc_range
 # from comp_P_ import comp_P_
-
 from filters import get_filters
-
 get_filters(globals())  # imports all filters at once
-
 from generic_branch import master_blob
 
 '''
     - this function is under revision
     - intra_blob() evaluates for recursive frame_blobs() and comp_P() within each blob
       combined with frame_blobs(), it forms a 2D version of first-level algorithm
-
+      
     - inter_subb() will compare sub_blobs of same range and derivation within higher-level blob, bottom-up ) top-down:
     - inter_level() will compare between blob levels, where lower composition level is integrated by inter_subb
       match between levels' edges may form composite blob, axis comp if sub_blobs within blob margin?
     - inter_blob() comparison will be second-level 2D algorithm, and a prototype for recursive meta-level algorithm
 '''
-
 
 def intra_blob_root(frame):  # simplified initial branch + eval_layer
 
@@ -33,18 +29,17 @@ def intra_blob_root(frame):  # simplified initial branch + eval_layer
                 rdn = 1
                 val_ = []
                 for sub_blob in blob.sub_blob_:
-                    if sub_blob.sign and sub_blob.params[-1] > ave_blob * 2:  # > variable and fixed costs
+                    if sub_blob.sign and sub_blob.params[-1] > ave_blob * 2:  # > variable and fixed costs of comp_angle
 
                         master_blob(sub_blob, comp_angle)
                         Ly, L, Y, X, I, Dx, Dy, G, A, Dax, Day, Ga = sub_blob.params
-                        # deeper eval per ablob core param: p | a rng or g | ga der:
+                        # next recursion vals:
 
+                        val_angle = Ga  # value of comp_ga -> gga, eval comp_angle(dax, day), next layer / aga_blob
                         val_deriv = ((G + ave * L) / ave * L) * -Ga  # relative G * -Ga: angle match, likely edge
                         val_range = G - val_deriv  # non-directional G: likely d reversal, distant-pixels match
-                        val_der_a = ((Ga + ave * L) / ave * L) * -Gga  # comp ga
-                        val_rng_a = Ga - val_der_a  # comp rng
 
-                        val_ += [(val_deriv, 0, sub_blob), (val_range, 1, sub_blob), (val_der_a, 2, sub_blob), (val_rng_a, 3, sub_blob)]
+                        val_ += [(val_angle, 0, sub_blob), (val_deriv, 1, sub_blob), (val_range, 2, sub_blob)]
                 if val_:
                     eval_layer(val_, rdn)
 
@@ -54,29 +49,20 @@ def intra_blob_root(frame):  # simplified initial branch + eval_layer
 def branch(blob, typ):  # compute branch per blob, evaluate for comp_angle, comp_inc_range, comp_inc_deriv, comp_P_
     vals = []
 
-    if typ == 0:
-        master_blob(blob, inc_range, 0)  # recursive comp over p_ of incremental distance
-    elif typ == 1:
-        master_blob(blob, inc_deriv, 0)  # recursive comp over g_ of incremental derivation
-    elif typ == 2:
-        master_blob(blob, inc_range, 1)  # recursive comp over a_ of incremental distance
-    else:
-        master_blob(blob, inc_deriv, 1)  # recursive comp over ga_ of incremental derivation
-
-    # added last 0 | 1 to select operand for a branch: p | a or g | ga, in the same dert
+    if typ == 0:   master_blob(blob, inc_deriv, 1)  # comp over ga_: last 0|1 selects a_dert vs. i_dert
+    elif typ == 1: master_blob(blob, inc_deriv, 0)  # recursive comp over g_ of incremental derivation
+    else:          master_blob(blob, inc_range, 0)  # recursive comp over i_ of incremental distance
 
     if blob.params[-1] > ave_blob * 2:  # if G > ave_blob * 2
         master_blob(blob, comp_angle)
-        Ly, L, Y, X, I, Dx, Dy, G, A, Dax, Day, Ga = sub_blob.params
+        Ly, L, Y, X, I, Dx, Dy, G, A, Dax, Day, Ga = blob.params
+        # next recursion vals:
 
-        # deeper eval per ablob core param: p | a rng or g | ga der:
-
+        val_angle = Ga  # value of comp_ga -> gga, eval comp_angle(dax, day), next layer / aga_blob
         val_deriv = ((G + ave * L) / ave * L) * -Ga  # relative G * -Ga: angle match, likely edge
         val_range = G - val_deriv  # non-directional G: likely d reversal, distant-pixels match
-        val_der_a = ((Ga + ave * L) / ave * L) * -Gga  # comp ga
-        val_rng_a = Ga - val_der_a  # comp rng
 
-        vals = [(val_deriv, 0, blob), (val_range, 1, blob), (val_der_a, 2, blob), (val_rng_a, 3, blob)]
+        vals = [(val_angle, 0, blob), (val_deriv, 1, blob), (val_range, 2, blob)]
         # estimated values per branch per blob
     return vals
 
@@ -108,32 +94,31 @@ def eval_layer(val_, rdn):  # val_: estimated values of active branches in curre
         eval_layer(sub_val_, rdn)  # evaluation of each sub_val for recursion
 
     ''' 
-        comp_P_(val, 0, blob, rdn) -> (val_PP_, 4, blob), (val_aPP_, 5, blob):
-        val_PP_ = (L + I + G) * (L / Ly / Ly) * (Dy / Dx)
-
+        comp_P_(val, 0, blob, rdn) -> (val_PP_, 4, blob), (val_aPP_, 5, blob),
+        val_PP_ = (L + I + G) * (L / Ly / Ly) * (Dy / Dx):
         1st term: proj P match Pm; Dx, Dy, abs_Dx, abs_Dy for scan-invariant hyp_g_P calc, comp, no indiv comp: rdn
         2nd term: elongation: >ave Pm? ~ box elong: (x_max - x_min) / (y_max - y_min)?
         3rd term: dimensional variation bias
-
         g and ga are dderived, blob selected for min_g
         val-= sub_blob and branch switch cost: added map?  only after g,a calc: no rough g comp?
     '''
     # ---------- eval_layer() end ---------------------------------------------------------------------------------------
 
 
-def hypot_g(blob):  # redefine blob and sub_blobs by reduced g and increased ave + ave_blob: var + fixed costs of angle_blobs() and eval
+def hypot_g(blob):  # redefine master blob by reduced g and increased ave * 2: variable costs of comp_angle
 
     mask = ~blob.map[:, :, np.newaxis].repeat(4, axis=2)  # stack map 4 times to fit the shape of dert__: (width, height, number of params)
     blob.new_dert__[0] = ma.array(blob.dert__, mask=mask)  # initialize dert__ with mask for selective comp
 
-    # redefine g = hypot(dx, dy), with incr filter = cost of angle calc
-    blob.new_dert__[0][:, :, 3] = np.hypot(blob.new_dert__[0][:, :, 1], blob.new_dert__[0][:, :, 2]) - ave * blob.ncomp * 2
+    # redefine g = hypot(dx, dy), ave * 2 assuming that added cost of angle calc = cost of hypot_g calc
+    blob.new_dert__[0][:, :, 3] = np.hypot(blob.new_dert__[0][:, :, 1], blob.new_dert__[0][:, :, 2]) - ave * 2
 
     return 1  # comp rng
 
 # ---------- hypot_g() end -----------------------------------------------------------------------------------
 
 def overlap(blob, box, map):  # returns number of overlapping pixels between blob.map and map
+
     y0, yn, x0, xn = blob.box
     _y0, _yn, _x0, _xn = box
 
