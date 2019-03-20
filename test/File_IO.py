4e365e405cfc5da6b4506c08bbf397c6809f5811
khanh93vn <khanh93vn@gmail.com>
2019-03-20 20:59:23 +0700

    Minor edits. Added comments on Read_File() and compare().

--- test/File_IO.py
+++ test/File_IO.py
@@ -1,13 +1,18 @@
 import cv2
 
 def Read_Image(path):
-    " Read image from file, return list of tuples of pixels and their coordinates "
+    ''' Read image from file, return list of tuples of pixels and their coordinates.
+        Input:  image file path
+        Output: list of (y, x, p):
+        - y, x: coordinate of pixel
+        - p: pixel's grayscale value (0->255)
+    '''
 
-    image = cv2.imread(path, 0).astype(int)
+    image = cv2.imread(path, 0).astype(int) # Read image with cv2
 
-    output = []
+    output_ = []                            # initialize output_
 
-    for y, p_ in enumerate(image):
-        output += [(y, x, p) for x, p in enumerate(p_)]
+    for y, p_ in enumerate(image):          # iterate through lines
+        output_ += [(y, x, p) for x, p in enumerate(p_)]
 
-    return ouput
+    return ouput_
--- test/Operations.py
+++ test/Operations.py
@@ -1,5 +1,20 @@
 def compare(input_, offset, comparand=2, axes=(0, 1)):
-    " Compare imputs over indicated offset "
+    ''' Compare imputs over indicated offset. Return differences between compared inputs.
+        - input_: list of inputs
+        - offset: offset between comparands.
+        - comparand: index of comparand in each input
+        - axes: indices of axes' coordinates of each input
+        Return: a list where each element is a list:
+            + First element is number of comps (not every input is compared twice)
+            + Remaining elements: difference value for every axis
+
+        For example: compare(input_, offset=(0, 1), comparand=2, axes=(0, 1))
+            With each input: (y, x, p):
+            + Coordinates: y, x (axes' indices = (0, 1))
+            + Comparand: p (comparand index = 2)
+            + Pure horizontal comp, with rng = 1: comp between input with coordinates: (y, x) and (y, x + 1)
+            (offset = (0, 1))
+            Return: each output: [num_comp, dy, dx] '''
 
     squared_mag = 0                         # squared magnitude of offset
     for d in offset:                        # offset between comparands
@@ -15,21 +30,25 @@ def compare(input_, offset, comparand=2, axes=(0, 1)):
     i = 0   # index of first comparand
     j = 0   # index of second comparand
 
-    while i < len(input_) and j < len(input_):  # this loop is per i
+    while i < len(input_) and j < len(input_):  # this loop is per i. j is updated accordingly. If either i or j is out of bound, no more comp could take place
 
         coord = [input_[i][axis] + offset[iaxis] for iaxis, axis in enumerate(axes)]    # coordinates of second comparand
+        comp = True
 
         for iaxis, axis in enumerate(axes):     # iterate through axes from most-significant to least-significant
             while j < len(input_) and input_[j][axis] < coord[iaxis]:
                 j += 1                          # increment j until conditions are met
 
-            if input_[j][axis] == coord[iaxis]:
-                temp = input_[j][comparand] - input_[i][comparand]  # compare
-                d =  [temp * coef for coef in coef_]                # decomposition into value for each axis
+            if input_[j][axis] != coord[iaxis]:     # if stuck on coordinate of one axis
+                pass                                # skip the rest
+                comp = False                        # do not comp (offset between input_[i] and input_[j] is not correct
 
-                output_[i] = [val1 + val2 for val1, val2 in zip(d + [1], output_[i])]   # bilateral accumulation
+        if comp:
+            temp = input_[j][comparand] - input_[i][comparand]  # compare by subtraction
+            d =  [temp * coef for coef in coef_]                # decomposition into value for each axis
 
-                output_[j] = [val1 + val2 for val1, val2 in zip(d + [1], output_[j])]   # bilateral accumulation
+            output_[i] = [val1 + val2 for val1, val2 in zip([1] + d, output_[i])]   # bilateral accumulation. Last element is number of comps
+            output_[j] = [val1 + val2 for val1, val2 in zip([1] + d, output_[j])]   # bilateral accumulation. Last element is number of comps
 
         i += 1
 
