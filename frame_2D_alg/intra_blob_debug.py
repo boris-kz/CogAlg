1ce159a58eb346d12808a73593d50fb118d5fb28
boris-kz <boris.kz@gmail.com>
2019-04-05 21:27:16 -0400

    converted to depth-first, no olp within a layer, still incomplete

--- frame_2D_alg/intra_blob_debug.py
+++ frame_2D_alg/intra_blob_debug.py
@@ -6,7 +6,7 @@ from comp_range import comp_range
 # from comp_P_ import comp_P_
 # from filters import get_filters
 # get_filters(globals())  # imports all filters at once
-from intra_comp_debug import master_blob
+from intra_comp_debug import unfold_blob, add_sub_blobs
 
 '''
     intra_blob() evaluates for recursive frame_blobs() and comp_P() within each blob.
@@ -27,13 +27,13 @@ from intra_comp_debug import master_blob
         sign, Y, X, Ly, L,  # these are common for all Derts, higher-Derts sign is always positive, else no branching 
         
         Derts = 
-            [ Dert = I, Dx, Dy, G ],  # one Dert per current and higher layers (higher if root blob is a sub_blob) 
+            [ Dert = I, Dx, Dy, G ],  # one Dert per current and lower layers of blob' derivation tree
         seg_ = 
             [ seg_params,  # formed in frame blobs
-              Py  # vertical buffer of Ps per segment
+              Py_ =  # vertical buffer of Ps per segment
                 [ P_params,
-                  derts = [ dert = i, dx, dy, g ]]],  # one dert per current and higher layers, 
-                  # for 2+ derts: typ instead of i and added ncomp?
+                  derts = [ dert = i|a, dx, dy, g ]]],  # one dert per current and higher (if root blob is a sub blob) layers 
+                  # for 2+ derts: add ncomp and blob typ instead of i?
                               
         sub_blob_ # sub_blob structure is same as in root blob 
             if len( sub_blob_) > min:
@@ -59,93 +59,34 @@ from intra_comp_debug import master_blob
     inter_blob() will be 2nd level 2D alg: a prototype for recursive meta-level alg
 '''
 
-def intra_blob_root(frame):  # simplified initial branch() and eval_layer() call
+def intra_blob_root(frame):  # also recursive_intra_blob(), called from add_sub_blobs(), if not comp_angle?
 
-    for top_blob in frame.blob_:
-        if top_blob.sign and top_blob.Derts[-1][-1] > ave_blob:
-            # g > ave: variable cost, and G > fixed cost of hypot_g: area of noisy or directional gradient
-            master_blob(top_blob, hypot_g, add_dert=False)  # redefines g as hypot(dx,dy), no Derts, derts extension
+    for blob in frame.blob_:
+        if blob.sign and blob.Derts[-1][-1] > ave_blob:  # G > fixed cost of sub blobs: area of noisy or directional gradient
+            add_sub_blobs(blob, hypot_g, add_dert=False)   # redefines g as hypot(dx,dy), no Derts, derts extension
 
-            if top_blob.Derts[-1][-1] > ave_blob * 2:  # G > fixed costs of comp_angle
-                val_ = []
-                for blob in top_blob.sub_blob_:  
+            if blob.Derts[-1][-1] > ave_blob * 2:  # G > fixed costs of comp_angle
+                for gblob in blob.sub_blob_:
 
-                    if blob.sign and blob.Derts[-1][-1] > ave_blob * 2:  # g > ave and G > fixed costs of comp_angle
-                        master_blob( blob, comp_angle)  # sub_blob = master ablob, no alt type rdn: no eval_layer
+                    if gblob.sign and gblob.Derts[-1][-1] > ave_blob * 2:  # g > ave and G > fixed costs of comp_angle
+                        unfold_blob( gblob, comp_angle, gaf=0, rdn=1)  # calls add_sub_blobs, but not recursive_intra_blob
 
-                        for ablob in blob.sub_blob_:  # eval / ablob: unique def / ga sign, vs rdn ablob_ || xblob_ if / gblob
-                            I, Dx, Dy, G = ablob.Derts[-2]  # Derts include params of all higher layers
-                            A, Dxa, Dya, Ga = ablob.Derts[-1]; Ave = ave * ablob.L
+                        for ablob in blob.sub_blob_:  # ablob is defined by ga sign
+                            I, Dx, Dy, G = ablob.Derts[-2]  # Derts: current and higher layers' params, no lower layers yet
+                            A, Dxa, Dya, Ga = ablob.Derts[-1]
+                            Ave = ave * ablob.L
+                            rdn = len(blob.sub_blob_) * 2
 
-                            val_angle_g = Ga  # value of comp_ga -> gga, eval comp_angle(dax, day), next layer / aga_blob
-                            val_gradient = ((G + Ave) / Ave) * -Ga   # relative G * - Ga: angle match, likely edge blob
-                            val_range = G - val_gradient  # non-directional G: likely reversal, distant-pixels match
+                            if Ga > ave_blob:   # value of comp_ga -> gga, eval comp_angle(dax, day), next layer / aga_blob
+                                unfold_blob( rdn, ablob, comp_gradient, gaf=1)
+                            else:
+                                val_gradient = ((G + Ave) / Ave) * -Ga   # relative G * - Ga: angle match, likely edge blob
+                                if val_gradient > ave_blob:
+                                    unfold_blob( rdn, ablob, comp_gradient, gaf=0)
+                                elif G - val_gradient > ave_blob:  # disoriented G: likely sign reversal and distant-pixels match
+                                    unfold_blob( rdn, ablob, comp_range, gaf=0)
 
-                            # estimated next-layer values per ablob:
-                            val_ += [(val_angle_g, 1, ablob, blob), (val_gradient, 2, ablob, blob), (val_range, 3, ablob, blob)]
-                if val_:
-                    eval_layer(val_, 2)  # rdn = 2: redundancy to higher-layer blob, or rdn = n ablobs: actual cost?
-                    # val_ is two merged layers: blob_ ( ablob_), for each top_blob
-
-    return frame  # frame of 2D patterns is output to level 2
-
-
-def comp_branch(blob, typ):  # branch-specific comp, evaluate next-layer branches: comp_angle, comp_ga, comp_deriv, comp_range
-    vals = []
-
-    if typ == 0:
-        master_blob(blob, comp_angle)  # blob will pass complete variable-depth sub_blob_ to its root_blob, and so on
-        for ablob in blob.sub_blob_:  # sub_blobs are defined by the sign of angle gradient deviation
-
-            I, Dx, Dy, G = ablob.Derts[-2]   # Derts include params of all higher layers
-            A, Dxa, Dya, Ga = ablob.Derts[-1]; Ave = ave * ablob.L  # to restore G from G_deviation for val_deriv:
-
-            val_angle_g = Ga  # value of comp_ga -> gga, eval comp_angle(dxa, dya), next layer / aga_blob
-            val_gradient = ((G + Ave) / Ave) * -Ga   # relative G * - Ga: angle match, likely edge blob
-            val_range = G - val_gradient  # non-directional G: likely reversal, distant-pixels match
-
-            # estimated next-layer values per ablob:
-            vals += [(val_angle_g, 1, ablob, blob), (val_gradient, 2, ablob, blob), (val_range, 3, ablob, blob)]
-    else:
-        if typ == 1:   master_blob(blob, comp_gradient, 1)  # comp over ga_: 1 selects aDert at Derts[-1]
-        elif typ == 2: master_blob(blob, comp_gradient, 0)  # comp over g_ with incremental derivation
-        else:          master_blob(blob, comp_range, 0)     # comp over i_ with incremental distance
-
-        for xblob in blob.sub_blob_:  # mixed-type sub_blobs, evaluated for comp_angle:
-            vals += [(xblob.Derts[-1][-1], 0, xblob, blob)]  # val_angle = G
-
-    return vals  # blob is converted into branch-specific root_blob, with added Derts[-1] and derts[-1]
-
-
-def eval_layer(val_, rdn):  # val_: estimated values of active branches in current layer across recursion tree per blob
-
-    val_ = sorted(val_, key=lambda val: val[0])
-    sub_val_ = []  # estimated branch values of a deeper layer of recursion tree per blob
-    map_ = []  # blob boxes + maps of stronger branches in val_, appended for next (lower) val evaluation
-
-    while val_:
-        val, typ, blob, root_blob = val_.pop()
-        # root_blob: blob per layer above blob, to assign sub_blob_ ref to add new layer to master blob
-        '''
-        for box, map in map_:  # of higher-value blobs in the layer, incrementally nested, with root_blobs per blob?
-            olp = overlap(blob, box, map)  # or no lateral overlap between xblobs?
-            rdn += 1 * (olp / blob.Derts[-1][-1])  # rdn += 1 * (olp / G):
-            # redundancy to higher and stronger-branch overlapping blobs, * specific branch cost ratio? '''
-
-        if val > ave * blob.L * rdn + ave_blob:  # val > ave * blob_area * rdn + fixed cost of master_blob per branch
-            map_ += [(blob.box, blob.map)]
-            for xblob in blob.sub_blob_:
-                sub_val_ += comp_branch(xblob, typ)  # from sub_blobs of intra_comp branch = typ
-        else:
-            break
-    if root_blob.layer_f:
-        root_blob.sub_blob_+= [(blob.sub_Derts, blob.sub_blob_)]  # convert sub_blob_ to layer_, new sub_blob_=[] in master_blob
-    else:
-        root_blob.sub_blob_ = [(blob.sub_Derts, blob.sub_blob_)]; root_blob.layer_f = 1
-
-    if sub_val_:
-        eval_layer(sub_val_, rdn+1)  # evaluate each sub_val for recursion within its sub_blob
-
+    # rdn: redundancy to higher-layer blob, *= n_stronger_sub_blobs for recursion?
     '''     
     comp_P_(val, 0, blob, rdn) -> (val_PP_, 4, blob), (val_aPP_, 5, blob),
     val_PP_ = 
@@ -153,11 +94,12 @@ def eval_layer(val_, rdn):  # val_: estimated values of active branches in curre
     * L/ Ly/ Ly: elongation: >ave Pm? ~ box elong: (xn - x0) / (yn - y0)? 
     * Dy / Dx:   variation-per-dimension bias 
     * Ave - Ga:  if positive angle match
-        
+
     g and ga are dderived, blob of min_g?
     val-= sub_blob and branch switch cost: added map?  only after g,a calc: no rough g comp?
     '''
-    # ---------- eval_layer() end ---------------------------------------------------------------------------------------
+
+    return frame  # frame of 2D patterns is output to level 2
 
 
 def hypot_g(blob):  # redefine master blob by reduced g and increased ave * 2: variable costs of comp_angle
@@ -172,6 +114,10 @@ def hypot_g(blob):  # redefine master blob by reduced g and increased ave * 2: v
 
     # ---------- hypot_g() end -----------------------------------------------------------------------------------
 
+ave = 20
+ave_blob = 200
+
+'''
 def overlap(blob, box, map):  # returns number of overlapping pixels between blob.map and map
 
     y0, yn, x0, xn = blob.box
@@ -194,7 +140,9 @@ def overlap(blob, box, map):  # returns number of overlapping pixels between blo
     olp = np.logical_and(map1, map2).sum()  # compute number of overlapping pixels
     return olp
 
-    # ---------- overlap() end ------------------------------------------------------------------------------------------
+for box, map in map_:  # of higher-value blobs in the layer, incrementally nested, with root_blobs per blob?
 
-ave = 20
-ave_blob = 200
+    olp = overlap(blob, box, map)  # or no lateral overlap between xblobs?
+    rdn += 1 * (olp / blob.Derts[-1][-1])  # rdn += 1 * (olp / G):
+    # redundancy to higher and stronger-branch overlapping blobs, * specific branch cost ratio? 
+'''
