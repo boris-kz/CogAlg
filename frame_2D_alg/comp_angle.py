f8b4f671a84a24f7f993f19215706bb2bb082263
Boris Kazachenko <boris.kz@gmail.com>
2019-05-11 20:59:17 -0400

    minor edits

--- frame_2D_alg/comp_angle.py
+++ frame_2D_alg/comp_angle.py
@@ -30,30 +30,30 @@ def lateral_comp(P_):  # horizontal comparison between pixels at distance == rng
         new_derts_ = []     # new derts buffer
 
         _derts = derts_[0]
-        gd, dx, dy, _ = _derts[-1]          # comp_angle always follows comp_gradient or hypot_g
-        _a = complex(dx, dy)                # to complex number: _a = dx + dyj
-        _a /= abs(_a)                       # normalize _a so that abs(_a) == 1 (hypot() from real and imaginary part of _a == 1)
-        _aa = phase(_a)                     # angular value of _a in radiant: _aa in (-pi, pi)
-        _dax, _ncomp = 0j, 0                # init ncomp, dx(complex) buffers
+        gd, dx, dy, _ = _derts[-1]         # comp_angle always follows comp_gradient or hypot_g
+        _a = complex(dx, dy)               # to complex number: _a = dx + dyj
+        _a /= abs(_a)                      # normalize _a so that abs(_a) == 1 (hypot() from real and imaginary part of _a == 1)
+        _a_radiant = phase(_a)             # angular value of _a in radiant: _a_radiant in (-pi, pi)
+        _dax, _ncomp = 0j, 0               # init ncomp, dx(complex) buffers
 
         for derts in derts_[1:]:
             # compute angle:
             g, dx, dy, _ = derts[-1]       # derts_ and new_derts_ are separate
             a = complex(dx, dy)            # to complex number: a = dx + dyj
             a /= abs(a)                    # normalize a so that abs(a) == 1 (hypot() from real and imaginary part of a == 1)
-            aa = phase(a)                  # angular value of a in radiant: aa in (-pi, pi)
+            a_radiant = phase(a)           # angular value of a in radiant: aa in (-pi, pi)
 
-            da = rect(1, aa - _aa)         # convert bearing difference into complex form (rectangular coordinate)
+            da = rect(1, a_radiant - _a_radiant)   # convert bearing difference into complex form (rectangular coordinate)
             # complex d doesn't need to correct angle diff: if d > pi: d -= 255; elif d < -127: d += 255
             dx = da
             _dax += da      # bilateral accumulation
             _ncomp += 1     # bilateral accumulation
 
-            new_derts_.append(_derts + [(_a, _aa), (0j, _dax, _ncomp)]) # return, with _dy = 0 + 0j and a in a separate tuple
-            _derts = derts                          # buffer derts
-            _a, _aa, _dx, _ncomp = a, aa, dax, 1    # buffer last ncomp and dx
+            new_derts_.append(_derts + [(_a, _a_radiant), (0j, _dax, _ncomp)])  # return a and _dy = 0 + 0j in separate tuples
+            _derts = derts                         # buffer derts
+            _a, _aa, _dx, _ncomp = a, a_radiant, dx, 1    # buffer last ncomp and dx
 
-        new_derts_.append(_derts + [(_a, _aa), (0j, _dax, _ncomp)]) # return last derts
+        new_derts_.append(_derts + [(_a, _a_radiant), (0j, _dax, _ncomp)]) # return last derts
 
         derts__.append((x0, new_derts_))    # new line of P derts_ appended with new_derts_
 
@@ -65,7 +65,7 @@ def vertical_comp(derts__, buff___):    # vertical comparison
 
     if not buff___:  # first line, if buff___ is empty
         _derts__ = []
-    else:               
+    else:
         _derts__ = buff___[0]
         scan_slice_(_derts__, derts__, i_index=(-2, 1), fangle=True)
 
