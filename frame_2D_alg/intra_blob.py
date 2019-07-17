08452ce92f2a78bfaa2f24b393cfe2de9a55ea1c
Boris Kazachenko <boris.kz@gmail.com>
2019-07-17 08:26:59 -0400

    minor edits, revert if needed

--- frame_2D_alg/intra_blob.py
+++ frame_2D_alg/intra_blob.py
@@ -7,6 +7,7 @@
     inter_level() will compare between blob levels, where lower composition level is integrated by inter_sub_blob
     match between levels' edges may form composite blob, axis comp if sub_blobs within blob margin?
     inter_blob() will be 2nd level 2D alg: a prototype for recursive meta-level alg
+
     Each intra_comp() call from intra_blob() adds a layer of sub_blobs, new dert to derts and Layer to Layers, in each blob.
     intra_comp also sends feedback to fork[fia][fder] in root_blob, then to root_blob.root_blob, etc.
     Blob structure:
@@ -23,19 +24,15 @@
         Py_ [(P_params, derts_)]  # vertical buffer of Ps per segment
         # derts [(g_dert, ga_dert)]: two layers per intra_blob, sum in blob.rng, i = derts[-1][fia]
         ],
-    derts__,   # intra_comp inputs
-    sub_blob_, # layer-sequential references down sub_blob derivation tree, sub_blob structure = blob structure
-    lLayers[   # summed reps of lower layers across sub_blob derivation tree, from feedback, for layer-parallel comp_blob
+    derts__,  # intra_comp inputs
+    Layers[ fork_tree [type, Dert, sub_blob_] ]  # Layers across derivation tree consist of forks: deriv+, range+, angle
 
-        Dert, forks,  # input g_rng+, a_rng+, derived gg_rng2, ga_rng2, fork id: f_deriv, f_angle
-        Dert, fforks, ... # fork_tree depth = Layers depth-1, += <= 8 (4 rng+, 4 der+) forks per Layer
-        # Dert may be None: params are summed only if min nforks, also summed fork_Layers if min nLayers?
-        ],
+        # fork_tree is nested to depth = Layers[n]-1, for layer-parallel comp_blob
+        # Dert may be None: params are summed if len(sub_blob_) > min, same for fork_ and fork_layer_?
+
     root_blob, # reference for feedback of all Derts params summed in sub_blobs
-    hLayers    # higher-Dert params += higher-dert params (including I), for feedback to root_blob, no forking
-'''
-
-import operator as op
+    hDerts     # higher-Dert params += higher-dert params (including I), for layer-parallel comp_blob, no forking
+    '''
 
 from collections import deque, defaultdict
 from functools import reduce
@@ -73,7 +70,7 @@ F_DERIV = 0b10
 # -----------------------------------------------------------------------------
 # Functions
 
-def intra_comp(i__, dert___, root_blob, Ave, rng, fork_type):
+def intra_comp(i__, dert___, root_blob, rng, fork_type, Ave, Ave_blob):
 
     # Take dert__ and i__ inside root_blob's box:
     i__ = i__[root_blob.slices]
@@ -84,20 +81,20 @@ def intra_comp(i__, dert___, root_blob, Ave, rng, fork_type):
     seg_ = deque()  # Buffer of running segments
     for y, P_ in enumerate(P__, start=y0):
         P_ = scan_P_(P_, seg_,
-                     form_blob_func=update_sub_blob,
+                     form_blob_func=merge_segment,
                      # update_sub_blob() addition arguments:
                      root_blob=root_blob, dert___=dert___,
                      rng=rng, fork_type=fork_type)
         seg_ = form_seg_(y, P_,
-                         form_blob_func=update_sub_blob,
+                         form_blob_func=merge_segment,
                          # update_sub_blob() addition arguments:
                          root_blob=root_blob, dert___=dert___,
                          rng=rng, fork_type=fork_type)
 
     # Merge last-line segments into their blobs:
-    while seg_:  form_blob(seg_.popleft(), root_blob, rng)
+    while seg_: form_blob(seg_.popleft(), root_blob, rng)
 
-    return Ave_blob
+    return Ave_blob * len(root_blob['Layers'][-1][fork_type]['sub_blob_']) / ave_n_sub_blobs
 
 
 def form_P_(x0, i_, dert_, Ave):
@@ -129,20 +126,19 @@ def form_P_(x0, i_, dert_, Ave):
 
 def form_P__(x0, i__, dert__, Ave):
     """
-    Form P across the whole dert array.
+    Forms Ps across the whole dert array.
     """
-    # Separating i__ vs. a__ cases:
-    if i__.ndim == 2: # Case of i__.
+    if i__.ndim == 2:   # inputs are g_derts
         dy_slice = 1
         dx_slice = 2
-    elif i__ndim == 3: # Case of a__.
+    elif i__.ndim == 3: # inputs are ga_derts
         dy_slice = slice(1, 3)
         dx_slice = slice(3, None)
     else:
         raise ValueError
-    g__ = dert__[0, :, :] # Assign g__ for partitioning.
+    g__ = dert__[0, :, :]  # g sign determines clustering:
 
-    # Partition:
+    # Clustering:
     s_x_L__ = starmap(lambda y, g_:
                       (y,
                        [(sign, next(group)[0], len(list(group)) + 1)
@@ -152,7 +148,7 @@ def form_P__(x0, i__, dert__, Ave):
                        ),
                       enumerate(g__))
 
-    # Clustering:
+    # Accumulation:
     P__ = [deque(dict(sign=s,
                       x0=x+x0,
                       I=i_[..., x : x+L].sum(axis=-1),
@@ -170,8 +166,8 @@ def form_P__(x0, i__, dert__, Ave):
     return P__
 
 
-def update_sub_blob(term_seg, root_blob, dert___, rng, fork_type):
-    """Terminated segment is merged into it's blob."""
+def merge_segment(term_seg, root_blob, dert___, rng, fork_type):
+
     y0, Is, Gs, Dys, Dxs, Ls, Lys, Py_, blob, roots = term_seg.values()
     I, G, Dy, Dx, L, Ly = blob['Dert'].values()
 
@@ -188,8 +184,8 @@ def update_sub_blob(term_seg, root_blob, dert___, rng, fork_type):
         terminate_blob(y0+Lys, blob, root_blob, dert___, rng, fork_type)
 
 
-def terminate_sub_blob(yn, blob, root_blob, dert___, rng, fork_type):
-    """Sub-blob is terminated and send feedback to it's root-blob."""
+def terminate_blob(yn, blob, root_blob, dert___, rng, fork_type):
+
     Dert, s, [y0, x0, xn], seg_, open_segs = blob.values()
 
     mask = np.ones((yn - y0, xn - x0), dtype=bool)  # local map of blob
@@ -219,10 +215,10 @@ def terminate_sub_blob(yn, blob, root_blob, dert___, rng, fork_type):
                 Layers={},
                 )
 
-    feed_back(blob)
+    feedback(blob)
 
 # -----------------------------------------------------------------------------
-def feed_back(blob):
+def feedback(blob):
 
     fork_type = blob['fork_type']
     root_blob = blob['root_blob']
@@ -316,32 +312,7 @@ def intra_blob(root_blob, rng, eval_fork_, Ave_blob, Ave):  # fia (flag ia) sele
     i rng+ / v(i+m), i & m = 0 if i is p, single exposed input per fork
     g der+ / vg: initial fork, but then more coarse?
     ga der+/ vga, rng = der+'rng? 
-
     no (G+Mg- ave_blob, 1, 1): comp a if same-rng comp i, no indep value, replaced by ga as i is replaced by g?
-
-    from comp_g -> mg, gg: summed over rng in same Dert, new Dert per der+ only 
-    m is unsigned, weak bias: no ma, mx,my, mA: lags ga? no kernel buff: olp, no Mg -> Mgg: g is not common 
-
-    dderived match beyond I and A: dev (min) g -> g_blob ( +match mg -> rg_blob, +miss gg -> gg_blob
-    val = G| Ga, overlap for higher Gg & Ga;  higher-level overlap is per weaker (der_order - re-order cost)? 
-
-    intra_comp returns Ave_blob *= len(blob.sub_blob_) / ave_n_sub_blobs  # adjust by actual / average n sub_blobs
-    ave and ave_blob *= fork coef, greater for coarse kernels, += input switch cost, or same for any new fork?
-
-    input mag is weakly predictive, comp at rng=1, only g is worth comparing at inc_range (inc_deriv per i_dert): 
-    derts[0] = 4i -> I feedback, not reinput? lower derts summation / comp?  comp_g(); eval? comp_angle (input g)
-    2x2 g comp initially & across weak blobs: negative G - Gg - Ga, shift in same direction, after intra_blob, comp_blob?  
-    all der+, if high Gg + Ga (0 if no comp_angle), input_g res reduction, convert to compare to 3x3 g?  
-
-    3x3 g comp within strong blobs (core param Mag + Match > Ave), forming concentric-g rng+ or der+ fork sub-blobs:       
-    rng+ blob def by ind g + mg, or g_blob-wide only, no sub-dif? contiguous rng+, no fb in P, sub_derts_ if min len?
-
-    comp_P eval per angle blob, if persistent direction * elongation? 
-    weak blob deconstruction for 2x2 rng+, no deconstruction of 3x3: rng+ stop only?
-
-    vertical comp val = proj proj match: feedback-skipping inc range: maximize novel, non-redundant projections?
-    kernel ave * ncomp (val= g_rate) * cost (ind * rdn): minor incr compared to clustering cost? 
-    simplicity vs efficiency: if fixed code+syntax complexity cost < accumulated variable inefficiency cost, in delays?
     '''
 
     return root_blob
