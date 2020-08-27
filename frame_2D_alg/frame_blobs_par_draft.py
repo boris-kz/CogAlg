a6542501828ef4724f2909cead824e72ab5b45b2
boris-kz <boris.kz@gmail.com>
2020-08-26 20:26:54 -0400

    - added delayed merge_blobs to divide expansion in cycles, - Khanh edited frame_blobs_c

--- frame_2D_alg/frame_blobs_par_draft.py
+++ frame_2D_alg/frame_blobs_par_draft.py
@@ -9,13 +9,21 @@ def frame_blobs_parallel(frame_dert__):
     pseudo code below:
     '''
     blob__ = frame_dert__  # initialize one blob per dert
-    blob__[:].append(frame_dert__[:])  # add open_derts = dert per blob
     frame_dert__[:].append(blob__[:])  # add blob ID to each dert, not sure expression is correct
+    for blob, dert in zip(blob__, frame_dert__):
+        blob = [blob]
+        blob += [[dert], []]  # add open_derts and empty connected_blobs to each blob
 
-    for blob in (blob__):  # initial blobs are open derts
+    # while open_derts: iterate blob extension cycle:
+    for blob in (blob__):  # single extension cycle, initial blobs are open derts
+
+        merge_blobs(blob, blob.connected_blobs)
+        # merge connected blobs added in last extension cycle, re-assign blob ID per dert
+        # connected blobs of these connected blobs are merged in next cycle, etc.:
+        blob.connected_blobs = []
         open_AND = 0  # counter of AND(open_dert.sign, unfilled_rim_dert.sign)
         new_open_derts = []
-        for dert in blob.open_derts:  # open border: list of derts in dert__ that have unfilled derts in their 3x3 rim
+        for dert in blob.open_derts:  # open_derts: list of derts in dert__ with unfilled derts in their 3x3 rim
             i = 0
             while i < 7:
                 _y, _x = compute_rim(dert.y, dert.x, i)  # compute rim _y _x from dert y x, clock-wise from top-left
@@ -25,43 +33,10 @@ def frame_blobs_parallel(frame_dert__):
                     new_open_derts.append(_dert)
                     if dert.sign and _dert.sign:
                         open_AND += 1
-                        merge_blobs(blob, _dert.blob)  # merge blob with blob currently assigned to _dert,
-                        # remaining vs. merged blob is the one prior in y ( x
-                        # new_open_derts += merged_blob_open_derts - overlap, re-assign blob ID per dert
-                        # remove merged blob from next extension cycle
+                        add_blobs(blob, _dert.blob)
+                        # blob.connected_blobs += blob currently assigned to _dert, excluding connected_blobs of that blob
+                        # remaining vs. added blob has lower blob ID, remove added blob from next cycle
+                        # new_open_derts += connected_blob.open_derts - overlap
         if open_AND==0:
             terminate_blob(blob)  # remove blob from next extension cycle
-
-        '''
-        old dert and box- based version:
-        concentric vs. y(x dert__ order for composition, oriented re-order for comp_P?
-        
-        rim_AND = 0  # counter of AND(last_rim_dert_sign, new_rim_dert_sign)
-
-        for j, _side in enumerate(blob.dert__[-1]):  # dert__ is a sequence of rims, each has four sides: top, right, bottom, left
-            if _side:  # array of dert signs, vs. [] if side has no blob-sign derts
-                if j==1:
-                    side = dert__[_side[0]-1, _side[1]]  # derts above top side, _side[0]: ys, _side[1]: xs
-                    direction = -1
-                elif j==2:
-                    side = dert__[_side[0], _side[1]+1]  # derts to the right of right side
-                    direction = 1
-                elif j==3:
-                    side = dert__[_side[0]+1, _side[1]]  # derts below bottom side
-                    direction = 1
-                else:
-                    side = dert__[_side[0], _side[1]-1]  # derts to the left of left side
-                    direction = -1
-                side_AND = 0
-                for (_sign, _dert), (sign, dert) in zip(_side[2], side[2]):
-                    if _sign and sign:
-                        side_AND += 1
-                        merge_blobs(blob, dert[-1])  # merge dert.blob into current blob
-                if side_AND:
-                    rim_AND += 1
-                    # blob.box[j-1] += direction  # extend y0 | yn | x0 | xn of blob.box
-                    # this should actually be part of merge_blobs: the box is extended by the box of merged blob?
-        if ~rim_AND:
-            terminate_blob(blob)        
-        '''
 
--- frame_2D_alg/frame_blobs_seq_c.py
+++ frame_2D_alg/frame_blobs_seq_c.py
@@ -1,121 +1,56 @@
-import ctypes
+from ctypes import *
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time
-from frame_blobs_yx import comp_pixel
+from frame_blobs_yx import comp_pixel, ave
 from utils import imread
-'''
-Khanh's version, implemented in C
-'''
-ave = 30  # filter or hyper-parameter, set as a guess, latter adjusted by feedback
 
-frame_blobs_parallel = ctypes.CDLL("frame_blobs_parallel.so").frame_blobs_parallel
+class DertRef(Structure):
+    _fields_ = [
+        ('x', c_int),
+        ('y', c_int),
+    ]
 
-img = imread('./images/raccoon.jpg')
-dert__ = [*map(lambda a: a.astype('float32'),
-               comp_pixel(img))]
-start_time = time()
-height, width = dert__[0].shape
-i = dert__[0].ctypes.data
-g = dert__[1].ctypes.data
-dy = dert__[2].ctypes.data
-dx = dert__[3].ctypes.data
-bmap = np.empty((height, width), 'uint8')
-nblobs = frame_blobs_parallel(i, g, dy, dx, height, width, ave,
-                              bmap.ctypes.data)
-print(f"{nblobs} blobs formed in {time() - start_time} seconds")
-plt.imshow(bmap, 'gray')
-plt.show()
+class Blob(Structure):
+    _fields_ = [
+        ('I', c_double),
+        ('G', c_double),
+        ('Dy', c_double),
+        ('Dx', c_double),
+        ('S', c_ulonglong),
+        ('sign', c_byte),
+        ('fopen', c_byte),
+        ('dert_ref', POINTER(DertRef)),
+    ]
 
-''' C code: '''
+class FrameOfBlobs(Structure):
+    _fields_ = [('blobs', POINTER(Blob)),
+                ('nblobs', c_ulong)]
 
-#include <stdio.h>
-#include <stdint.h>
-#include <malloc.h>
-#include <string.h>
-#define QUEUE_MAX_LEN   2000    /* max queue length */
+derts2blobs = CDLL("frame_blobs.so").derts2blobs
+derts2blobs.restype = FrameOfBlobs
 
-typedef struct {
-    float I, G, Dy, Dx, S;
-    char sign, fopen;
-} struct_blob;
+if __name__ == "__main__":
+    import argparse
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument('-i', '--image', help='path to image file', default='./images//raccoon_eye.jpg')
+    argument_parser.add_argument('-v', '--verbose', help='print details, useful for debugging', type=int, default=1)
+    argument_parser.add_argument('-n', '--intra', help='run intra_blobs after frame_blobs', type=int, default=0)
+    argument_parser.add_argument('-r', '--render', help='render the process', type=int, default=1)
+    arguments = vars(argument_parser.parse_args())
+    image = imread(arguments['image'])
+    verbose = arguments['verbose']
+    intra = arguments['intra']
+    render = arguments['render']
 
-int adj_offset[8][2] = {
-    {-1, 0},    /* top */
-    {0, 1},     /* right */
-    {1, 0},     /* bottom */
-    {0, -1},    /* left */
-    {-1, -1},   /* top-left */
-    {-1, 1},    /* top-right */
-    {1, 1},     /* btm-right */
-    {1, -1},    /* btm-left */
-};
+    start_time = time()
+    dert__ = [*map(lambda a: a.astype('float64'),
+                   comp_pixel(image))]
+    height, width = dert__[0].shape
+    idmap = np.empty((height, width), 'uint32')
+    frame = derts2blobs(*map(lambda d: d.ctypes.data, dert__),
+                        height, width, ave, idmap.ctypes.data)
 
-int frame_blobs_parallel(float *i_, float *g_, float *dy_, float *dx_,
-                         int height, int width, int ave,
-                         uint8_t *bmap) {
-    static int queue[QUEUE_MAX_LEN],    /* a queue for FIFO data */
-               qbeg, qend;
-    int nblobs = 0,                     /* total number of blobs */
-        size = height * width;          /* total number of derts */
-    char *fill_map;                     /* id_map to track flood fill */
-
-    // initialize fill_map
-    fill_map = (char*) malloc(size * sizeof(char));
-    memset(fill_map, 0, sizeof(fill_map));
-
-    // Loop through all derts
-    for(int i = 0; i < size; i++)
-        if(!fill_map[i]) {  /* ignore filled derts */
-            fill_map[i] = 1;
-            nblobs++;
-            float I = 0, G = 0, Dy = 0, Dx = 0, S = 0;
-            char sign = g_[i] - ave > 0,
-                 fopen = 0;
-
-            // do flood fill
-            qbeg = 0;
-            qend = 1;
-            queue[qbeg] = i;
-            while(qbeg != qend) {
-                int j = queue[qbeg++];           /* pop last dert's index */
-                if(qbeg >= QUEUE_MAX_LEN) qbeg = 0;
-                // TODO: add coordinate container
-                I += i_[j];
-                G += g_[j];
-                Dy += dy_[j];
-                Dx += dx_[j];
-                S += 1;
-                bmap[j] = sign?255:0;
-
-                int y = j / width,
-                    x = j % width;
-                // loop through adjacent coordinates, 8 if sign else 4
-                for(int dir = 0; dir < (sign?8:4); dir++) {
-                    int y2 = y + adj_offset[dir][0],
-                        x2 = x + adj_offset[dir][1];
-                    // check if image boundary is reached
-                    if(y2 < 0 || y2 >= height ||
-                       x2 < 0 || x2 >= width) fopen = 1;
-                    else {
-                        int k = y2 * width + x2;
-                        // ignore filled
-                        if(fill_map[k]) continue;
-                        // check if same-signed
-                        if(sign == (g_[k] - ave > 0)) {
-                            fill_map[k] = 1;
-                            queue[qend++] = k;  /* append this hash */
-                            if(qend >= QUEUE_MAX_LEN) qend = 0;
-                        }
-                        // else assign adjacents
-                        else {
-                            // TODO: assign adjacents
-                        }
-                    }
-                }
-            }
-        }
-    free(fill_map);
-
-    return nblobs;
-}
+    print(f"{frame.nblobs} blobs formed in {time() - start_time} seconds")
+    plt.imshow(idmap, 'gray')
+    plt.show()
