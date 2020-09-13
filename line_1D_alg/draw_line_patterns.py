695ad6a501d25668c98b0bef50b1b5ca02a19788
boris-kz <boris.kz@gmail.com>
2020-09-13 19:40:57 -0400

    updates from Chee, minor edits

--- line_1D_alg/draw_line_patterns.py
+++ line_1D_alg/draw_line_patterns.py
@@ -7,11 +7,11 @@ import argparse
 from time import time
 from utils import *
 from itertools import zip_longest
-from line_PPs_draft import comp_P, form_PPm
-from line_patterns import cross_comp
-
+from line_PPs_draft import comp_P_, form_PPm
+from line_patterns_class import cross_comp
 
 def draw_frame_patterns(image, arguments, frame_of_patterns_):
+
     # initialize image
     img_ini = np.zeros_like(image).astype('float')
     img_p = img_ini.copy()
@@ -24,51 +24,59 @@ def draw_frame_patterns(image, arguments, frame_of_patterns_):
     img_mM = img_ini.copy()
 
     # drawing section
-    for y, P_ in enumerate(frame_of_patterns_):  # loop each y line
-        x = 0  # x location start with 0 in each line
+    for y, P_ in enumerate(frame_of_patterns_): # loop each y line
+        x = 0 # x location start with 0 in each line
 
-        for P_num, P in enumerate(P_[0]):  # loop each pattern
+        for P_num, P in enumerate(P_[0]): # loop each pattern
 
-            colour_S = 0  # negative sign, colour = black (0)
-            if P[0]:
-                colour_S = 255  # if positive sign, colour = white (255)
-            colour_I = P[2]  # I
-            colour_D = P[3]  # D
-            colour_M = P[4]  # M
+            colour_S = 0 # negative sign, colour = black (0)
+            if P.sign:
+                colour_S = 255 # if positive sign, colour = white (255)
+            colour_I = P.I # I
+            colour_D = P.D # D
+            colour_M = P.M # M
 
-            for dert_num, (p, d, m) in enumerate(P[5]):  # loop each dert
-                if d is None:  # set None to 0
+            for dert_num, dert in enumerate(P.dert_): # loop each dert
+                p,d,m = dert.p, dert.d, dert.m
+                if d is None: # set None to 0
                     d = 0
 
-                img_p[y, x] = p  # dert's p
-                img_d[y, x] = d  # dert's d
-                img_m[y, x] = m  # dert's m
-                img_mS[y, x] = colour_S  # 1st layer mP's sign
-                img_mI[y, x] = colour_I  # 1st layer mP's I
-                img_mD[y, x] = colour_D  # 1st layer mP's D
-                img_mM[y, x] = colour_M  # 1st layer mP's M
+                img_p[y,x] = p # dert's p
+                img_d[y,x] = d # dert's d
+                img_m[y,x] = m # dert's m
+                img_mS[y,x] = colour_S # 1st layer mP's sign
+                img_mI[y,x] = colour_I # 1st layer mP's I
+                img_mD[y,x] = colour_D # 1st layer mP's D
+                img_mM[y,x] = colour_M # 1st layer mP's M
 
-                x += 1
+                x+=1
 
     # normalize value (scale from min to max) on negative or big numbers
-    # remove 4 lines below to observe non-normalized images
-    img_d = (img_d - img_d.min()) / (img_d.max() - img_d.min()) * 255
-    img_m = (img_m - img_m.min()) / (img_m.max() - img_m.min()) * 255
-    img_mI = (img_mI - img_mI.min()) / (img_mI.max() - img_mI.min()) * 255
-    img_mD = (img_mD - img_mD.min()) / (img_mD.max() - img_mD.min()) * 255
-    img_mM = (img_mM - img_mM.min()) / (img_mM.max() - img_mM.min()) * 255
+    img_d_norm = (img_d - img_d.min()) /(img_d.max()-img_d.min() )*255
+    img_m_norm = (img_m - img_m.min()) /(img_m.max()-img_m.min() )*255
+    img_mI_norm = (img_mI - img_mI.min()) /(img_mI.max()-img_mI.min() )*255
+    img_mD_norm = (img_mD - img_mD.min()) /(img_mD.max()-img_mD.min() )*255
+    img_mM_norm = (img_mM - img_mM.min()) /(img_mM.max()-img_mM.min() )*255
 
     # save images to disk
-    cv2.imwrite(arguments['output_path'] + 'p.jpg', img_p.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'd.jpg', img_d.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'm.jpg', img_m.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'mPS.jpg', img_mS.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'mPI.jpg', img_mI.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'mPD.jpg', img_mD.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'mPM.jpg', img_mM.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '1_p.jpg',  img_p.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '2_d.jpg',  img_d.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '4_m.jpg',  img_m.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '6_mPS.jpg',  img_mS.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '7_mPI.jpg',  img_mI.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '9_mPD.jpg',  img_mD.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '11_mPM.jpg',  img_mM.astype('uint8'))
+
+    # save normalized images to disk
+    cv2.imwrite(arguments['output_path'] + '3_d_norm.jpg',  img_d_norm.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '5_m_norm.jpg',  img_m_norm.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '8_mPI_norm.jpg',  img_mI_norm.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '10_mPD_norm.jpg',  img_mD_norm.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '12_mPM_norm.jpg',  img_mM_norm.astype('uint8'))
 
 
 def draw_frame_dert_P(image, arguments, frame_of_patterns_):
+
     # initialize image
     img_ini = np.zeros_like(image).astype('float')
     img_smP = img_ini.copy()
@@ -84,44 +92,73 @@ def draw_frame_dert_P(image, arguments, frame_of_patterns_):
     img_DM = img_ini.copy()
 
     # drawing section
-    for y, dert_P_ in enumerate(frame_of_dert_P_):  # loop each y line
+    for y, dert_P_ in enumerate(frame_of_dert_P_): # loop each y line
 
         x = 0
-        for dert_P_num, dert_P in enumerate(dert_P_):  # loop each dert_P
+        for dert_P_num, dert_P in enumerate(dert_P_): # loop each dert_P
 
-            smP, MP, Neg_M, Neg_L, ML, DL, MI, DI, MD, DD, MM, DM, _ = dert_P  # get dert_P params
+            # get dert_P params
+            smP, MP, Neg_M, Neg_L, ML, DL, MI, DI, MD, DD, MM, DM = dert_P.smP, dert_P.MP, dert_P.Neg_M, dert_P.Neg_L, dert_P.ML, dert_P.DL, dert_P.MI, dert_P.DI, dert_P.MD, dert_P.DD, dert_P.MM, dert_P.DM
+
 
             # accumulate parameters for visualization
             # there are overlapping Ps, so Ps with more overlapping should get more values
             for i in range(Neg_L):
-                img_smP[y, x + i] += smP
-                img_MP[y, x + i] += MP
-                img_Neg_M[y, x + i] += Neg_M
-                img_ML[y, x + i] += ML
-                img_DL[y, x + i] += DL
-                img_MI[y, x + i] += MI
-                img_DI[y, x + i] += DI
-                img_MD[y, x + i] += MD
-                img_DD[y, x + i] += DD
-                img_MM[y, x + i] += MM
-                img_DM[y, x + i] += DM
+
+                img_smP[y,x+i] += smP
+                img_MP[y,x+i] += MP
+                img_Neg_M [y,x+i] += Neg_M
+                img_ML[y,x+i] += ML
+                img_DL[y,x+i] += DL
+                img_MI[y,x+i] += MI
+                img_DI[y,x+i] += DI
+                img_MD[y,x+i] += MD
+                img_DD[y,x+i] += DD
+                img_MM[y,x+i] += MM
+                img_DM[y,x+i] += DM
 
             # loop each Ps' dert to get next x starting location
-            for dert_num, (p, d, m) in enumerate(dert_P[12][5]):
-                x += 1
+            for dert_num, dert in enumerate(dert_P.P.dert_):
+                x+=1
 
-    cv2.imwrite(arguments['output_path'] + 'smP.jpg', img_smP.astype('uint8') * 255)
-    cv2.imwrite(arguments['output_path'] + 'MP.jpg', img_MP.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'Neg_M.jpg', img_Neg_M.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'ML.jpg', img_ML.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'DL.jpg', img_DL.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'MI.jpg', img_MI.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'DI.jpg', img_DI.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'MD.jpg', img_MD.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'DD.jpg', img_DD.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'MM.jpg', img_MM.astype('uint8'))
-    cv2.imwrite(arguments['output_path'] + 'DM.jpg', img_DM.astype('uint8'))
 
+
+    # normalize value (scale from min to max) on negative or big numbers
+    img_MP_norm = (img_MP - img_MP.min()) /(img_MP.max()-img_MP.min() )*255
+    img_Neg_M_norm = (img_Neg_M - img_Neg_M.min()) /(img_Neg_M.max()-img_Neg_M.min() )*255
+    img_ML_norm = (img_ML - img_ML.min()) /(img_ML.max()-img_ML.min() )*255
+    img_DL_norm = (img_DL - img_DL.min()) /(img_DL.max()-img_DL.min() )*255
+    img_MI_norm = (img_MI - img_MI.min()) /(img_MI.max()-img_MI.min() )*255
+    img_DI_norm = (img_DI - img_DI.min()) /(img_DI.max()-img_DI.min() )*255
+    img_MD_norm = (img_MD - img_MD.min()) /(img_MD.max()-img_MD.min() )*255
+    img_DD_norm = (img_DD - img_DD.min()) /(img_DD.max()-img_DD.min() )*255
+    img_MM_norm = (img_MM - img_MM.min()) /(img_MM.max()-img_MM.min() )*255
+    img_DM_norm = (img_DM - img_DM.min()) /(img_DM.max()-img_DM.min() )*255
+
+    # save images to disk
+    cv2.imwrite(arguments['output_path'] + '13_smP.jpg',  img_smP.astype('uint8')*255)
+    cv2.imwrite(arguments['output_path'] + '14_MP.jpg',  img_MP.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '16_Neg_M.jpg',  img_Neg_M.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '18_ML.jpg',  img_ML.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '20_DL.jpg',  img_DL.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '22_MI.jpg',  img_MI.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '24_DI.jpg',  img_DI.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '26_MD.jpg',  img_MD.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '28_DD.jpg',  img_DD.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '30_MM.jpg',  img_MM.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '32_DM.jpg',  img_DM.astype('uint8'))
+
+    # save normalized images to disk
+    cv2.imwrite(arguments['output_path'] + '15_MP_norm.jpg',  img_MP_norm.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '17_Neg_M_norm.jpg',  img_Neg_M_norm.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '19_ML_norm.jpg',  img_ML_norm.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '21_DL_norm.jpg',  img_DL_norm.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '23_MI_norm.jpg',  img_MI_norm.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '25_DI_norm.jpg',  img_DI_norm.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '27_MD_norm.jpg',  img_MD_norm.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '29_DD_norm.jpg',  img_DD_norm.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '31_MM_norm.jpg',  img_MM_norm.astype('uint8'))
+    cv2.imwrite(arguments['output_path'] + '33_DM_norm.jpg',  img_DM_norm.astype('uint8'))
 
 if __name__ == "__main__":
     # Parse argument (image)
@@ -148,17 +185,21 @@ if __name__ == "__main__":
         frame_of_dert_P_ = []
         frame_of_PPm_ = []
         for y, P_ in enumerate(frame_of_patterns_):
-            dert_P_ = comp_P(P_[0])
-            PPm_ = form_PPm(dert_P_)
+            dert_P_ = comp_P_(P_[0])
+            if len(dert_P_)>1:
+                PPm_ = form_PPm(dert_P_)
+            else:
+                PPm_ = []
 
             frame_of_dert_P_.append(dert_P_)
             frame_of_PPm_.append(PPm_)
 
-            # draw frame of patterns
+    # draw frame of patterns
     draw_frame_patterns(image, arguments, frame_of_patterns_)
 
     # draw frame of dert P
     draw_frame_dert_P(image, arguments, frame_of_dert_P_)
+
 
     end_time = time() - start_time
     print(end_time)
--- line_1D_alg/line_PPs_draft.py
+++ line_1D_alg/line_PPs_draft.py
@@ -24,13 +24,223 @@ Resulting PPs will be more like 1D graphs, with explicit distances between neare
 This is different from 1st level connectivity clustering, where all distances between nearest elements = 1.
 '''
 import numpy as np
+from class_cluster import ClusterStructure, NoneType
 
 ave_dI = 1000
 ave_div = 50
-ave_rM = .5   # average relative match per input magnitude, at rl=1 or .5?
+ave_rM = .5  # average relative match per input magnitude, at rl=1 or .5?
 ave_net_M = 100  # search stop
 ave_Ls = 3
+
 # no ave_mP: deviation computed via rM  # ave_mP = ave*3: comp cost, or n vars per P: rep cost?
+
+class Cdert_P(ClusterStructure):
+    smP = NoneType
+    MP = int
+    Neg_M = int
+    Neg_L = int
+    P = list
+    ML = int
+    DL = int
+    MI = int
+    DI = int
+    MD = int
+    DD = int
+    MM = int
+    DM = int
+
+class CPP(ClusterStructure):
+    smP = NoneType
+    MP = int
+    Neg_M = int
+    Neg_L = int
+    P_ = list
+    ML = int
+    DL = int
+    MI = int
+    DI = int
+    MD = int
+    DD = int
+    MM = int
+    DM = int
+
+
+def comp_P_(P_):  # cross-compare patterns within horizontal line
+    dert_P_ = []  # comp_P_ forms array of alternating-sign dert_Ps (derivatives + P): output of pair-wise comp_P
+
+    for i, P in enumerate(P_):
+        neg_M = vmP = smP = _smP = neg_L = 0  # initialization
+        M = P.M
+        for j, _P in enumerate(P_[i + 1:]):  # variable-range comp, no last-P displacement, just shifting first _P
+            if M - neg_M > ave_net_M:
+                # search while net_M > ave, True for 1st _P, no select by M sign
+                dert_P, _L, _smP = comp_P(P, _P, neg_M, neg_L)
+                smP, vmP, neg_M, neg_L, P = dert_P.smP, dert_P.MP, dert_P.Neg_M, dert_P.Neg_L, dert_P.P
+                if smP:
+                    P_[i + 1 + j].smP = True  # backward match per P: __smP = True
+                    dert_P_.append(dert_P)
+                    break  # nearest-neighbour search is terminated by first match
+                else:
+                    neg_M += vmP  # accumulate contiguous miss: negative mP
+                    neg_L += _L  # accumulate distance to match
+                    if j == len(P_):  # last P
+                        dert_P_.append(Cdert_P(smP=smP or _smP, MP=vmP, Neg_M=neg_M, Neg_L=neg_L, P=P,
+                                               ML=0, DL=0, MI=0, DI=0, MD=0, DD=0, MM=0, DM=0))  # unconnected P, derivatives are ignored
+                    '''                     
+                    no contrast value in neg dert_Ps and PPs: initial opposite-sign P miss is expected
+                    neg_dert_P derivatives are not significant; neg_M obviates distance * decay_rate * M '''
+            else:
+                dert_P_.append(Cdert_P(smP=smP or _smP, MP=vmP, Neg_M=neg_M, Neg_L=neg_L, P=P, ML=0, DL=0, MI=0, DI=0, MD=0, DD=0, MM=0, DM=0))
+                # smP is ORed bilaterally, negative for single (weak) dert_Ps
+                break  # neg net_M: stop search
+
+    return dert_P_
+
+
+def comp_P(P, _P, neg_M, neg_L):
+    # _smP = 0 in line_patterns, M: deviation even if min
+    sign, L, I, D, M, dert_, sub_H, _smP = P.sign, P.L, P.I, P.D, P.M, P.dert_, P.sub_layers, P.smP
+    _sign, _L, _I, _D, _M, _dert_, _sub_H, __smP = _P.sign, _P.L, _P.I, _P.D, _P.M, _P.dert_, _P.sub_layers, _P.smP
+
+    dL = L - _L
+    mL = min(L, _L)  # - ave_rM * L?  L: positions / sign, derived: magnitude-proportional value
+    dI = I - _I  # proportional to distance, not I?
+    mI = ave_dI - abs(dI)  # I is not derived, match is inverse deviation of miss
+    dD = D - _D  # sum if opposite-sign
+    mD = min(D, _D)  # - ave_rM * D?  same-sign D in dP?
+    dM = M - _M  # sum if opposite-sign
+    mM = min(M, _M)  # - ave_rM * M?  negative if x-sign, M += adj_M + deep_M: P value before layer value?
+
+    mP = mL + mM + mD  # match(P, _P) for derived vars, mI is already a deviation
+    proj_mP = (L + M + D) * (ave_rM ** (1 + neg_L / L))  # projected mP at current relative distance
+    vmP = mI + (mP - proj_mP)  # deviation from projected mP, ~ I*rM contrast value, +|-? replaces mP?
+    smP = vmP > 0
+
+    if smP:  # forward match, compare sub_layers between P.sub_H and _P.sub_H (sub_hierarchies):
+        dert_sub_H = []
+        if P.sub_layers and _P.sub_layers:  # not empty sub layers
+            for sub_P, _sub_P in zip(P.sub_layers, _P.sub_layers):
+                if P and _P:  # both forks exist
+                    Ls, fdP, fid, rdn, rng, sub_P_ = sub_P[0]
+                    _Ls, _fdP, _fid, _rdn, _rng, _sub_P_ = _sub_P[0]
+                    # fork comparison:
+                    if fdP == _fdP and rng == _rng and min(Ls, _Ls) > ave_Ls:
+                        dert_sub_P_ = []
+                        sub_MP = 0
+                        # compare all sub_Ps to each _sub_P, form dert_sub_P per compared pair
+                        for sub_P in sub_P_:
+                            for _sub_P in _sub_P_:
+                                dert_sub_P, _, _ = comp_P(sub_P, _sub_P, neg_M=0, neg_L=0)  # ignore _sub_L, _sub_smP?
+                                sub_MP += dert_sub_P.MP  # sum sub_vmPs in dert_P_layer
+                                dert_sub_P_.append(dert_sub_P)
+
+                        dert_sub_H.append((fdP, fid, rdn, rng, dert_sub_P_))  # only layers that have been compared
+                        vmP += sub_MP  # of compared H, no specific mP?
+                        if sub_MP < ave_net_M:
+                            # or mH: trans-layer induction?
+                            break  # low vertical induction, deeper sub_layers are not compared
+                    else:
+                        break  # deeper P and _P sub_layers are from different intra_comp forks, not comparable?
+
+    dert_P = Cdert_P(smP=smP, MP=vmP, Neg_M=neg_M, Neg_L=neg_L, P=P, ML=mL, DL=dL, MI=mI, DI=dI, MD=mD, DD=dD, MM=mM, DM=dM)
+    return dert_P, _L, _smP
+
+
+def form_PPm(dert_P_):  # cluster dert_Ps by mP sign, positive only: no contrast in overlapping comp?
+    PPm_ = []
+    # initialize PPm with first dert_P:
+    dert_P = dert_P_[0]
+     # positive only, no contrast?
+    _smP, MP, Neg_M, Neg_L, _P, ML, DL, MI, DI, MD, DD, MM, DM = \
+        dert_P.smP, dert_P.MP, dert_P.Neg_M, dert_P.Neg_L, dert_P.P, \
+        dert_P.ML, dert_P.DL, dert_P.MI, dert_P.DI, dert_P.MD, dert_P.DD, dert_P.MM, dert_P.DM
+    P_ = [_P]
+
+    for i, dert_P in enumerate(dert_P_, start=1):
+        smP = dert_P.smP
+        if smP != _smP:
+            PPm_.append(CPP(smP=smP, MP=MP, Neg_M=Neg_M, Neg_L=Neg_L, P_=P_, ML=ML, DL=DL,MI=MI, DI=DI, MD=MD, DD=DD, MM=MM, DM=DM))
+            # initialize PPm with current dert_P:
+            _smP, MP, Neg_M, Neg_L, _P, ML, DL, MI, DI, MD, DD, MM, DM = \
+                dert_P.smP, dert_P.MP, dert_P.Neg_M, dert_P.Neg_L, dert_P.P, \
+                dert_P.ML, dert_P.DL, dert_P.MI, dert_P.DI, dert_P.MD, dert_P.DD, dert_P.MM, dert_P.DM
+            P_ = [_P]
+        else:
+            # accumulate PPm with current dert_P:
+            smP, mP, neg_M, neg_L, P, mL, dL, mI, dI, mD, dD, mM, dM = \
+                dert_P.smP, dert_P.MP, dert_P.Neg_M, dert_P.Neg_L, dert_P.P, \
+                dert_P.ML, dert_P.DL, dert_P.MI, dert_P.DI, dert_P.MD, dert_P.DD, dert_P.MM, dert_P.DM
+            MP+=mP; Neg_M+=neg_M; Neg_L+=neg_L; ML+=mL; DL+=dL; MI+=mI; DI+=dI; MD+=mD; DD+=dD; MM+=mM; DM+=dM
+
+            P_.append(P)
+        _smP = smP
+    # pack last PP
+    PPm_.append(CPP(smP=_smP, MP=MP, Neg_M=Neg_M, Neg_L=Neg_L, P_=P_, ML=ML, DL=DL,MI=MI, DI=DI, MD=MD, DD=DD, MM=MM, DM=DM))
+
+    return PPm_
+    # in form_PPd:
+    # dP = dL + dM + dD  # -> directional PPd, equal-weight params, no rdn?
+    # ds = 1 if Pd > 0 else 0
+
+def div_comp_P(PP_):  # draft, check all PPs for div_comp among their element Ps
+    '''
+    evaluation for comp by division is per PP, not per P: results must be comparable between consecutive Ps
+    estimated value of division = rm * D, compression = D - min * N: vertical vs lateral. if same-sign:
+
+    * higher-derivation D: ratio induction?
+    * higher-composition D: vertical d-induction from L, div value?
+    sum comp -> rVar | ave comp: Var*rL -> dVar?
+    '''
+    for PP in PP_:
+        if PP.M / (PP.L + PP.I + abs(PP.D) + abs(PP.dM)) * (abs(PP.dL) + abs(PP.dI) + abs(PP.dD) + abs(PP.dM)) > ave_div:
+            # if irM * D_vars: match rate projects der and div match,
+            # div if scale invariance: comp x dVars, signed
+            ''' 
+            | abs(dL + dI + dD + dM): div value ~= L, Vars correlation: stability of density, opposite signs cancel-out?
+            | div_comp value is match: min(dL, dI, dD, dM) * 4, | sum of pairwise mins?
+            '''
+            _dert_P = PP.dert_P_[0]
+            # smP, vmP, neg_M, neg_L, iP, mL, dL, mI, dI, mD, dD, mM, dM = P,
+            _sign, _L, _I, _D, _M, _dert_, _sub_H, __smP = _dert_P[4]
+
+            for i, dert_P in enumerate(PP.dert_P_[1:]):
+                sign, L, I, D, M, dert_, sub_H, _smP = dert_P[4]
+                # DIV comp L, SUB comp (summed param * rL) -> scale-independent d, neg if cross-sign:
+                rL = L / _L
+                # mL = whole_rL * min_L?
+                dI = I * rL - _I  # rL-normalized dI, vs. nI = dI * rL or aI = I / L
+                mI = ave_dI - abs(dI)  # I is not derived, match is inverse deviation of miss
+                dD = D * rL - _D  # sum if opposite-sign
+                mD = min(D, _D)   # same-sign D in dP?
+                dM = M * rL - _M  # sum if opposite-sign
+                mM = min(M, _M)   # - ave_rM * M?  negative if x-sign, M += adj_M + deep_M: P value before layer value?
+
+                mP = mI + mM + mD  # match(P, _P) for derived vars, mI is already a deviation
+                                   # defines norm_mPP, no ndx: single, but nmx is summed
+                if mP > dert_P[1]:
+                    rrdn = 1  # added to rdn, or diff alt, olp, div rdn?
+                else:
+                    rrdn = 2
+                if mP > ave_dI * 3 * rrdn:
+                    rvars = mP, mI, mD, mM, dI, dD, dM  # dPP_rdn, ndPP_rdn
+                else:
+                    rvars = []
+                # append rrdn and ratio variables to current dert_P:
+                PP.dert_P_[i] += [rrdn, rvars]
+                # P vars -> _P vars:
+                _sign = sign, _L = L, _I = I, _D = D, _M = M, _dert_ = dert_, _sub_H = sub_H, __smP = _smP
+                '''
+                also define dP,
+                if Pd > Pnd: ndPP_rdn = 1; dPP_rdn = 0  # value = D | nD
+                else:        dPP_rdn = 1; ndPP_rdn = 0
+                '''
+    return PP_
+
+
+''' non-class versions: '''
+
+def accum_PP(PP: dict, **params) -> None:
+    PP.update({param: PP[param] + value for param, value in params.items()})
 
 
 def comp_P_(P_):  # cross-compare patterns within horizontal line
@@ -139,60 +349,3 @@ def form_PPm(dert_P_):  # cluster dert_Ps by mP sign, positive only: no contrast
 
 def accum_PP(PP: dict, **params) -> None:
     PP.update({param: PP[param] + value for param, value in params.items()})
-
-
-def div_comp_P(PP_):  # draft, check all PPs for div_comp among their element Ps
-    '''
-    evaluation for comp by division is per PP, not per P: results must be comparable between consecutive Ps
-    estimated value of division = rm * contig D, compressible by subtracting multiple of min: vertically vs. laterally.
-
-    * higher D-orders?  division is compression of D, ratio match ~ N same-sign d-orders?
-    * D of composition order: induction to lower orders?
-
-    higher comp to normalize for difference in L: higher-composition param
-    sum comp -> rVar | ave comp: Var*rL -> dVar?
-    '''
-    for PP in PP_:
-        if PP.M / (PP.L + PP.I + abs(PP.D) + abs(PP.dM)) * (abs(PP.dL) + abs(PP.dI) + abs(PP.dD) + abs(PP.dM)) > ave_div:
-            # if irM * D_vars: match rate projects der and div match,
-            # div if scale invariance: comp x dVars, signed
-            ''' 
-            | abs(dL + dI + dD + dM): div value ~= L, Vars correlation: stability of density, opposite signs cancel-out?
-            | div_comp value is match: min(dL, dI, dD, dM) * 4, | sum of pairwise mins?
-            '''
-            _dert_P = PP.dert_P_[0]
-            # smP, vmP, neg_M, neg_L, iP, mL, dL, mI, dI, mD, dD, mM, dM = P,
-            _sign, _L, _I, _D, _M, _dert_, _sub_H, __smP = _dert_P[4]
-
-            for i, dert_P in enumerate(PP.dert_P_[1:]):
-                sign, L, I, D, M, dert_, sub_H, _smP = dert_P[4]
-                # DIV comp L, SUB comp (summed param * rL) -> scale-independent d, neg if cross-sign:
-                rL = L / _L
-                # mL = whole_rL * min_L?
-                dI = I * rL - _I  # rL-normalized dI, vs. nI = dI * rL or aI = I / L
-                mI = ave_dI - abs(dI)  # I is not derived, match is inverse deviation of miss
-                dD = D * rL - _D  # sum if opposite-sign
-                mD = min(D, _D)   # same-sign D in dP?
-                dM = M * rL - _M  # sum if opposite-sign
-                mM = min(M, _M)   # - ave_rM * M?  negative if x-sign, M += adj_M + deep_M: P value before layer value?
-
-                mP = mI + mM + mD  # match(P, _P) for derived vars, mI is already a deviation
-                                   # defines norm_mPP, no ndx: single, but nmx is summed
-                if mP > dert_P[1]:
-                    rrdn = 1  # added to rdn, or diff alt, olp, div rdn?
-                else:
-                    rrdn = 2
-                if mP > ave_dI * 3 * rrdn:
-                    rvars = mP, mI, mD, mM, dI, dD, dM  # dPP_rdn, ndPP_rdn
-                else:
-                    rvars = []
-                # append rrdn and ratio variables to current dert_P:
-                PP.dert_P_[i] += [rrdn, rvars]
-                # P vars -> _P vars:
-                _sign = sign, _L = L, _I = I, _D = D, _M = M, _dert_ = dert_, _sub_H = sub_H, __smP = _smP
-                '''
-                also define dP,
-                if Pd > Pnd: ndPP_rdn = 1; dPP_rdn = 0  # value = D | nD
-                else:        dPP_rdn = 1; ndPP_rdn = 0
-                '''
-    return PP_
