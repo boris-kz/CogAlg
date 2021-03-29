1e108e18a648fb8925aa67280d54d452286c27ca
boris-kz <boris.kz@gmail.com>
2021-03-28 20:21:29 -0400

    Chee's updates

--- frame_2D_alg/comp_slice_flip.py
+++ frame_2D_alg/comp_slice_flip.py
@@ -340,6 +340,18 @@ def form_PP_shell(blob, derP__, P__, derPd__, Pd__, fPPd):
             derP_2_PP_(blob.derP__, blob.PPmm_, 1, 0)   # cluster by derPm mP sign
             derP_2_PP_(blob.derPd__, blob.PPmd_, 1, 0)  # cluster by derPd mP sign
 
+        # assign spliced_PP after forming all PPs and FPPs
+        PPs_ = [blob.PPdm_,blob.PPdd_,blob.PPmm_,blob.PPmd_]
+        for PP_ in PPs_:
+            for PP in PP_:
+                # check 'if FPP' to save on below?
+                # splice FPP with connected PPs:
+                for derP in PP.xflip_derP_:  # check derPs where flip_val changed sign
+                    _P = derP._P
+                    if _P.derP.PP not in PP.xflip_derP_PP_:  # add _PP to P's PP
+                        PP.xflip_derP_PP_.append(_P.derP.PP)
+#                    if FPP not in _P.derP.PP.xflip_derP_PP_:  # add PP to _P's PP
+#                        _P.derP.PP.xflip_derP_PP_.append(PP)
     else:
         FPP = blob  # reassign for clarity
         FPP.derPf__ = derP__; FPP.Pf__ = P__
@@ -350,12 +362,6 @@ def form_PP_shell(blob, derP__, P__, derPd__, Pd__, fPPd):
         else:
             derP_2_PP_(FPP.derPf__, FPP.PPmmf_, 0, 0)   # cluster by derPmf mP sign
             derP_2_PP_(FPP.derPdf__, FPP.PPmdf_, 0, 0)  # cluster by derPdf mP sign
-
-        # splice FPP with connected PPs:
-        for derP in FPP.xflip_derP_:  # check derPs where flip_val changed sign
-            _P = derP._P
-            if FPP not in _P.derP.PP.splice_PP_:  # add PP to _P's PP
-                _P.derP.PP.splice_PP_.append(FPP)
 
 
 def derP_2_PP_(derP_, PP_, fflip, fPPd):
@@ -511,6 +517,11 @@ def accum_PP(PP, derP):  # accumulate derP params in PP
 
     Dert = derP.P.Dert
     # accumulate Dert params
+    ''' use:
+    for param, PP_param in zip(Dert, PP.Dert):
+        PP_param+=param
+    ?
+    '''
     PP.Dert.accumulate(I=Dert.I, Dy=Dert.Dy, Dx=Dert.Dx, G=Dert.G, M=Dert.M, Dyy=Dert.Dyy, Dyx=Dert.Dyx, Dxy=Dert.Dxy, Dxx=Dert.Dxx,
                      Ga=Dert.Ga, Ma=Dert.Ma, Mdx=Dert.Mdx, Ddx=Dert.Ddx, flip_val=Dert.flip_val)
     # accumulate derP params
@@ -520,8 +531,7 @@ def accum_PP(PP, derP):  # accumulate derP params in PP
 
     derP.PP = PP  # update reference
 
-    # this should be done in comp_slice?:
-    if derP.xfflip: # add splice point
+    if derP.fxflip: # add splice point
         PP.xflip_derP_.append(derP)
 
 def comp_dx(P):  # cross-comp of dx s in P.dert_
@@ -571,8 +581,7 @@ def comp_slice(_P, P, _derP_):  # forms vertical derivatives of derP params, and
 
     # if flip value>0 AND positive mP (predictive value) AND flip_val sign changed AND _P.derP is derP: exclude 1st row Ps
     if (P.Dert.flip_val>0) and (derP.mP >0) and ((P.Dert.flip_val>0) != (_P.Dert.flip_val>0)) and (isinstance(_P.derP, CderP)):
-        # buffer splicing points:
-        derP.PP.xflip_derP_.append(derP)
+        derP.fxflip = 1
 
     return derP
 
@@ -649,8 +658,7 @@ def comp_slice_full(_P, P):  # forms vertical derivatives of derP params, and co
 
     # if flip value>0 AND positive mP (predictive value) AND flip_val sign changed AND _P.derP is derP: exclude 1st row Ps
     if (P.Dert.flip_val>0) and (derP.mP >0) and ((P.Dert.flip_val>0) != (_P.Dert.flip_val>0)) and (isinstance(_P.derP, CderP)):
-        # buffer splicing points:
-        derP.PP.xflip_derP_.append(derP)
+        derP.fxflip = 1
 
     if fdx:
         derP.fdx=1; derP.dDdx=dDdx; derP.mDdx=mDdx; derP.dMdx=dMdx; derP.mMdx=mMdx
