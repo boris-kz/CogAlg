317b4c877b71c64ebf4e76ed125d8232407292a4
boris-kz <boris.kz@gmail.com>
2021-04-18 20:39:51 -0400

    Chee's updates with edits.

--- frame_2D_alg/comp_blob_draft.py
+++ frame_2D_alg/comp_blob_draft.py
@@ -1,38 +1,116 @@
 '''
 Cross-compare blobs with incremental adjacency, within a frame
 '''
+
+from class_cluster import ClusterStructure, NoneType
+
+class CderBlob(ClusterStructure):
+
+    _blob = object
+    blob = object
+    dI = int
+    mI = int
+    dA = int
+    mA = int
+    dG = int
+    mG = int
+    dM = int
+    mM = int
+    mB = int
+    dB = int
+    sub_derBlob = list  # why sub, this should already be packed in sub_blobs?
+
+class CBblob(ClusterStructure):
+
+    derBlob = object
+    derBlob_ = list
+    blob_ = list
+
 ave_M = 100
 ave_mP = 100
+ave_dP = 100
+
 
 def cross_comp_blobs(blob_):
     '''
     root function of comp_blob: cross compare blobs with their adjacent blobs in frame.blob_, including sub_layers
     '''
+    derBlob__ = []
     for blob in blob_:
 
         checked_ids_ = [blob.id]; net_M = 0  # checked ids per blob: may be checked from multiple root blobs
-        # between blobs:
-        comp_blob_recursive(blob, blob.adj_blobs[0], checked_ids_, net_M)
+        derBlob__ += comp_blob_recursive(blob, blob.adj_blobs[0], checked_ids_, net_M)  # comp between blob, blob.adj_blobs
+
+    bblob_ = form_bblob_(derBlob__)
+
+    return bblob_
+
+# draft, similar with derP_2_PP
+def form_bblob_(derBlob__):
+    '''
+    form bblob with same sign derBlob
+    '''
+    bblob_ = []
+    checked_ids = []
+
+    for derBlob in derBlob__[1:]:
+        if derBlob.id not in checked_ids: # current derBlob is not checked before
+            checked_ids.append(derBlob.id)
+
+            bblob = CBblob(derBlob=CderBlob()) # init new bblob
+            accum_bblob(bblob,derBlob)         # accum derBlob into bblob
+
+            if derBlob._blob.derBlob.id not in checked_ids: # current derBlob is not checked before
+                form_bblob_recursive(bblob, derBlob, derBlob._blob.derBlob, checked_ids) # recursively add blob to bblob via the adjacency
+
+            bblob_.append(bblob) # pack bblob after checking through all adjacents
+
+    return(bblob_)
+
+
+def form_bblob_recursive(bblob, _derBlob, derBlob, checked_ids):
+    '''
+    As distinct from form_PP_, consecutive blobs don't have to be adjacent, that needs to be checked through blob.adj_blobs
+    '''
+
+    checked_ids.append(derBlob.id)
+
+    if (_derBlob.mB>0) == (derBlob.mB>0):        # same sign check for adjacent derBlob
+        accum_bblob(bblob,derBlob._blob.derBlob) # accum same sign derBlob into bblob
+
+        if derBlob._blob.derBlob.id not in checked_ids: # current derBlob is not checked before
+            form_bblob_recursive(bblob, derBlob, derBlob._blob.derBlob, checked_ids)  # recursively add blob to bblob via the adjacency
+
+
+def accum_bblob(bblob, derBlob):
+
+    # accumulate derBlob
+    bblob.derBlob.accumulate(**{param:getattr(derBlob, param) for param in bblob.derBlob.numeric_params})
+    # add derBlob and blob to list
+    bblob.derBlob_.append(derBlob)
+    bblob.blob_.append(derBlob.blob)
 
 
 def comp_blob_recursive(blob, adj_blob_, checked_ids_, net_M):
     '''
     called by cross_comp_blob to recursively compare blob to adj_blobs in incremental layers of adjacency
     '''
+    derBlob_ = []
+
     for adj_blob in adj_blob_:
         if adj_blob.id not in checked_ids_:
 
-            comp_blob(blob,adj_blob)          # cross compare blob and adjacent blob
-            net_M += adj_blob.Dert.M          # cost for extending the cross comparison
+            derBlob = comp_blob(blob,adj_blob)  # cross compare blob and adjacent blob
+            net_M += adj_blob.Dert.M          # cost of extending the cross comparison
             checked_ids_.append(adj_blob.id)
-            # not sure about this, need further review:
-            # search adjacents of adjacent if crit, depth-first
-            if blob.Dert.M - net_M > ave_M:
-                comp_blob_recursive(blob, adj_blob.adj_blobs[0], checked_ids_, net_M)
+            derBlob_.append(derBlob)
 
-            if blob.fsliced and adj_blob.fsliced:
-                # apply comp_PP here to PPmm, Ppdm, PPmd, PPdd
-                pass
+            if blob.Dert.M - net_M > ave_M:  # if crit: search adjacents of adjacent, depth-first
+                derBlob_ += comp_blob_recursive(blob, adj_blob.adj_blobs[0], checked_ids_, net_M)
+
+            # if blob.fsliced and adj_blob.fsliced: this should be in comp_blob
+
+    return derBlob_
 
 
 def comp_blob(_blob, blob):
@@ -50,25 +128,23 @@ def comp_blob(_blob, blob):
     mG = min(_G, G)
     dM = _M - M
     mM = min(_M, M)
-    mP = mI + mA + mG + mM
+    mB = mI + mA + mG + mM
+    dB = dI + dA + dG + dM
 
-    if mP > ave_mP:
-        # form derblob here?
-        # yes, thanks!
+    # form derBlob regardless
+    derBlob  = CderBlob(_blob=_blob, blob=blob, dI=dI, mI=mI, dA=dA, mA=mA, dG=dG, mG=mG, dM=dM, mM=mM, mB=mB, dB=dB)
+    blob.derBlob = derBlob
 
-        for _sub_layer, sub_layer in zip(_blob.sub_layers, blob.sub_layers):
+    return derBlob
 
-            cross_comp_blobs(_sub_layer[0])  # sub_blobs, add more conditions?
-            cross_comp_blobs(sub_layer[0])  # could be done per blob only, but we need to do it before comp layers
+'''
+    cross-comp among sub_blobs in nested sub_layers:
 
-            if _sub_layer[0].fork == sub_layer[0].fork:  # add compare other layer-wide parameters?
-                _sub_layer[0].append([sub_layer[0]])  # merge layers: temporary, unmerge if low cross-sub_blobs match?
-                cross_comp_blobs(_sub_layer[0])  # between sub blobs of same-depth layers of _blob and blob, skip same-blob cross-comp
-                # we need additional conditions in cross_comp_blobs for that?
-            
+    _sub_layer = bblob.sub_layer[0]
+    for sub_layer in bblob.sub_layer[1:]:
+        for _sub_blob in _sub_layer:
+            for sub_blob in sub_layer:
+                comp_blob(_sub_blob, sub_blob)
 
-if __name__ == "__main__":
-    # Imports
-    import argparse
-    from time import time
-    from utils import imread
+        merge(_sub_layer, sub_layer)  # only for sub-blobs not combined into new bblobs by cross-comp
+'''
