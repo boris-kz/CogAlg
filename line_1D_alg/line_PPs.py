9ef158992440b9f7a33ff8b5b48bc9d783dad0e0
boris-kz <boris.kz@gmail.com>
2021-09-09 20:59:31 -0400

    minor edits

--- line_1D_alg/line_PPs.py
+++ line_1D_alg/line_PPs.py
@@ -3,7 +3,7 @@ line_PPs is a 2nd-level 1D algorithm, its input is P_ formed by the 1st-level li
 It cross-compares P params (initially L, I, D, M) and forms param_Ps: Pp_ for each param type per image row.
 -
 Subsequent cross-comp between Pps of different params is exclusive of x overlap, where the relationship is already known.
-Thus it should be on 3rd level: no Pp overlap means comp between Pps: higher composition, same-type or cross-type?
+Thus it should be on 3rd level: no Pp overlap means comp between Pps: higher composition, same-type ) cross-type?
 '''
 
 import sys  # add CogAlg folder to system path
@@ -91,7 +91,7 @@ def search(P_, fPd):  # cross-compare patterns within horizontal line
         frac_rL = max(rL, 1/rL) - int_rL
         mL = int_rL * min(L, _L) - (int_rL*frac_rL) / 2 - ave_mL  # div_comp match is additive compression: +=min, not directional
         Ldert_.append(Cdert( i=L, p=L + _L, d=rL, m=mL))
-        # sub_comp for summed params:
+        # summed params comp:
         if fPd:
             Idert_ += [comp_param(_I, I, "I_", ave_mI)]
             Ddert = comp_param(_D, D2, "D_", ave_mD)  # step=2 for same-D-sign comp
@@ -128,7 +128,7 @@ def search(P_, fPd):  # cross-compare patterns within horizontal line
             Ppm_ = form_Pp_rng(None, Pdert_, rdn_, P_)
         else:
             Ppm_ = form_Pp_(None, Pdert_, param_name, rdn_, P_, fPd=0)  # Ppd_ is formed in -Ppms only, in intra_Ppm_
-        # rval_Pp_s per param:
+        # redundant-value sign-spans of Pps per param:
         rval_Pp__ += [ form_rval_Pp_(Ppm_, param_name, dert1_, dert2_, fPd=0)]  # evaluates for compact()
         Ppm__ += [Ppm_]
 
@@ -153,9 +153,8 @@ def search_param_(P_, ave, rave):  # variable-range search in mdert_, only if pa
             curr_M = pdert.m * rave + (_P.M + P.M) / 2  # P.M is bidirectional
 
             if curr_M > ave_sub:  # comp all sub_P_ params, for core I only?
-                comp_sublayers_draft(P_[i], P_[j], pdert)  # should set dert.sub_M
-                # or default comp of summed params only?
-            if curr_M + pdert.sub_M > ave_M:  # match between sublayers; or > ave_cM?
+                comp_sublayers_draft(P_[i], P_[j], pdert)  # should set dert.sub_M: between sublayers
+            if curr_M + pdert.sub_M > ave_M:  # or > ave_cM? else pdert.sub_M is negative
                 break  # 1st match takes over connectivity search in the next loop
             else:
                 pdert.negM += curr_M - ave_M  # known to be negative, accum per dert
@@ -358,11 +357,11 @@ def intra_Pp_(Pp_, param_name, fPd):  # evaluate for sub-recursion in line Pm_, 
 
             if fPd:  # Pp is Ppd
                 if abs(Pp.D) * mean_M > ave_D * Pp.Rdn and Pp.L > 3:  # mean_M from adjacent +ve Ppms
-
-                    sub_search_draft(Pp.P_, fPd)  # search in top sublayer, also depends on pdert.m, eval per P?
+                    # search in top sublayer, eval by pdert.d:
+                    sub_search_draft(Pp.P_, fPd)
                     rdn_ = [rdn + 1 for rdn in Pp.rdn_[:-1]]
                     ddert_ = []
-                    # higher derivation comp:
+                    # higher derivation comp, if sublayers.D?
                     for _pdert, pdert in zip( Pp.pdert_[:-1], Pp.pdert_[1:]):  # Pd.pdert_ is dert1_
                         _param = _pdert.d; param = pdert.d
                         dert = comp_param(_param, param, param_name[0], ave)  # cross-comp of ds in dert1_, !search, local aves?
@@ -372,7 +371,8 @@ def intra_Pp_(Pp_, param_name, fPd):  # evaluate for sub-recursion in line Pm_, 
 
             else:  # Pp is Ppm
                 if Pp.M > 0 and Pp.M > ave_M * Pp.Rdn and param_name=="I_":  # and if variable cost: Pp.M / Pp.L? -lend to contrast?
-                    sub_search_draft(Pp.P_, fPd)  # search in top sublayer
+                    # search in top sublayer, eval by pdert.m:
+                    sub_search_draft(Pp.P_, fPd)
                     # +Ppm -> sub_Ppm_: low-variation span, eval rng_comp:
                     rdn_ = [rdn+1 for rdn in Pp.rdn_[:-1]]
                     P_ave = Pp.M / Pp.L  # also I,D,M /= L?
@@ -392,8 +392,7 @@ def intra_Pp_(Pp_, param_name, fPd):  # evaluate for sub-recursion in line Pm_, 
     return comb_layers
 
 
-def sub_search_draft(P_, fPd):  # search in top sublayer per P / sub_P,
-    # after P_ search: top-down induction,
+def sub_search_draft(P_, fPd):  # search in top sublayer per P / sub_P, after P_ search: top-down induction,
     # called from intra_Pp_, especially MPp: init select by P.M, then combined Pp match?
 
     for P in P_:
@@ -406,20 +405,21 @@ def sub_search_draft(P_, fPd):  # search in top sublayer per P / sub_P,
                     if abs(P.D) > ave_D:  # better use sublayers.D|M, but we don't have it yet
                         sub_rdn_Pp__ = search(sub_P_, fPd)
                         sublayer[4].append(sub_rdn_Pp__)
-                        # no direct recursion, only as mediated by intra_Pp_:
-                        # sub_search_recursive(sub_P_, fPd)  # deeper sublayers search is selective per sub_P
+                        # no direct recursion: sub_search_recursive(sub_P_, fPd)  # deeper sublayers search is selective per sub_P
 
                 elif P.M > ave_M:  # + pdert.m?
                     sub_rdn_Pp__ = search(sub_P_, fPd)
                     sublayer[4].append(sub_rdn_Pp__)
-                    # sub_search_recursive(sub_P_, fPd)  # deeper sublayers search is selective per sub_P
+                    # no direct recursion: sub_search_recursive(sub_P_, fPd)  # deeper sublayers search is selective per sub_P
 
 
 def comp_sublayers_draft(_P, P, pdert):
-    # conditional on pdert.m, summed params m, also positional m: mx0?
+    # if pdert.m -> if summed params m -> if positional m: mx0?
+    dist_coef = 2  # converts distance between sub_Ps into negative projected match
 
     if P.sublayers and _P.sublayers:  # not empty sub layers
         for _sub_layer, sub_layer in zip(_P.sublayers[0], P.sublayers[0]):
+            # comp summed params
             # accumulate dert.sub_M
             if _sub_layer and sub_layer:
                 _fPd, _rdn, _rng, _sub_P_, _sub_Pp__, = _sub_layer
@@ -427,16 +427,13 @@ def comp_sublayers_draft(_P, P, pdert):
                 # fork comparison:
                 if fPd == _fPd and rng == _rng and min(_P.L, P.L) > ave_Ls:
                     # compare sub_Ps to each _sub_P within max distance, comb_M- proportional:
-                    sub_search_param_(_sub_P_, sub_P_, pdert)
-
-                    '''
                     for _sub_P in _sub_P_:
                         for sub_P in sub_P_:
-                            if (_sub_P.M + sub_P.M) / 2 + pdert.m: # something like that
+                            if (_sub_P.M + sub_P.M) / 2 + pdert.m > (sub_P.x0 - (_sub_P.x0 + _sub_P.L)) * dist_coef:  # actually max_x0 - min_x0
+                               # something like that, tentative
                                 sub_dert = comp_param(_sub_P.I, sub_P.I, "I_", ave_mI)
                                 pdert.sub_M += sub_dert.m  # between whole compared sub_Hs
                                 pdert.sub_D += sub_dert.d
-                    '''
 
                     if pdert.sub_M + pdert.m + P.M < ave_sub_M:  # combine match values across all P levels.
                         break  # low vertical induction, deeper sublayers are not compared
