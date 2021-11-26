9a4dc0f384875b5ff72fbd47544280f6e77e82ea
boris-kz <boris.kz@gmail.com>
2021-11-25 20:35:07 -0500

    minor edits

--- line_1D_alg/increment.py
+++ line_1D_alg/increment.py
@@ -59,4 +59,50 @@ def form_Pp_incr(form_Pp_):
     pass
 
 
+# draft
+def line_PPP_root(Pp_ttt):  # higher-level input is nested to the depth = 2*elevation (level counter), or 2^elevation?
+
+    Pp_ttttt = []  # add Pp vars tuple ) Pppm, Pppd tuple
+
+    norm_feedback(P_t)  # before processing
+
+    for fPd, P_ in enumerate(P_t):  # fPd: Pm_ or Pd_
+        if len(P_) > 1:
+            Pdert_t, dert1_, dert2_ = cross_comp(P_, fPd)  # Pdert_t: Ldert_, Idert_, Ddert_, Mdert_
+            Pp_tt = []  # Ppm_t, Ppd_t, each: [LPp_, IPp_, DPp_, MPp_]
+
+            for fPpd in 0, 1:  # 0-> Ppm_t, 1-> Ppd_t
+                Pp_t = []  # [LPp_, IPp_, DPp_, MPp_]
+                rdn_t = sum_rdn_(param_names, Pdert_t, fPd)
+                # Pdert_-> Pps:
+                for param_name, Pdert_, rdn_ in zip(param_names, Pdert_t, rdn_t):
+                    Pp_ = form_Pp_(Pdert_, fPpd)
+                    if (fPpd and param_name == "D_") or (not fPpd and param_name == "I_"):
+                        if not fPpd:
+                            splice_Ps(Pp_, dert1_, dert2_, fPd)  # splice eval by Pp.M in Ppm_, for Pms in +IPpms or Pds in +DPpm
+                        Pp_ = intra_Pp_(None, Pp_, Pdert_, 1, fPpd)  # der+ or rng+
+                    Pp_t.append(Pp_)
+                Pp_tt.append(Pp_t)
+            Pp_ttt.append(Pp_tt)
+        else:
+            Pp_ttt.append(P_)  # Pps are not formed
+
+    return Pp_ttt  # 3-level nested tuple per line: Pm_, Pd_( Ppm_, Ppd_( LPp_, IPp_, DPp_, MPp_)))
+
+
+# draft
+def fom_PPP_(Pp_):
+    # form PPP_ from Pp_ here
+    pass
+
+# draft
+def form_PPP_recursive(Pp_ttt):
+    Ppp_tttttt = form_PPP_root(Pp_ttt)
+
+    if f_recursive:
+        return form_PPP_recursive(PPP_ttt)
+    else:
+        return PPP_ttt
+
+
 
