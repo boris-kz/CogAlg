c6b799cff49059fcd3377589abfa900ae6b5e24c
boris-kz <boris.kz@gmail.com>
2024-07-03 22:26:49 -0400

    edits with Chee

--- frame_2D_alg/vectorize_edge_blob/agg_recursion.py
+++ frame_2D_alg/vectorize_edge_blob/agg_recursion.py
@@ -91,34 +91,31 @@ def vectorize_root(image):  # vectorization in 3 composition levels of xcomp, cl
                 edge.node_ = node_t; edge.link_ = link_t
                 # edge.node_ may be node_tt: node_t per fork?
 
+
 def agg_recursion(root, N_, fL, rng=1):  # fL: compare node-mediated links, else < rng-distant CGs
 
     N_, Et, rng = rng_link_(N_) if fL else rng_node_(N_, rng)  # each is rng-recursive
     node_t = form_graph_t(root, N_, Et, rng)  # rng for sub+, sub_Gs += fback_
     if node_t:
         for fd, node_ in zip((0,1), node_t):
-            N_ = [n for n in node_ if n.derH.Et[0] > G_aves[fd] * n.derH.Et[2]]  # pruned node_
-            # comp val is proportional to n comparands:
+            N_ = [n for n in node_ if n.derH.Et[fd] > G_aves[fd] * n.derH.Et[2+fd]]
+            # prune node_
             if root.derH.Et[0] * (max(0,(len(N_)-1)*root.rng)) > G_aves[1]*root.derH.Et[2]:
-                agg_recursion(root, N_, fL=0)  # form_graph_t forms CGs
-                # agg+ / node_
+                # agg+: rng+ val *= n comparands, forms CGs:
+                agg_recursion(root, N_, fL=0)
         root.node_[:] = node_t
     # else keep root.node_
 
 def rng_node_(_N_, rng):  # forms discrete rng+ links, vs indirect rng+ in rng_kern_, still no sub_Gs / rng+
 
     while True:
-        N_, Et = rng_kern_(_N_, rng)  # incr rng
+        for G in _N_:
+            G.extH.H += [CH()]; G.compared__ += [[]]
+        N_, Et = rng_kern_(_N_, rng)  # += rng layer
         if Et[0] > ave * Et[2]:
-            for G in N_:
-                if G.DerH:  # formed in rng_kern_
-                    G.extH.append_(G.DerH, flat=0); G.DerH = CH()  # or immediately use G.extH?
-                    G.compared__ += [[]]
-                    N_ += [G]
-                else: delattr(G, "krim")
-            rng += 1
-            _N_ = N_
+            rng += 1; _N_ = N_
         else:
+            for G in _N_: G.extH.H.pop()  # low-value extH layer
             break
     return N_, Et, rng
 
@@ -126,9 +123,9 @@ def rng_kern_(N_, rng):  # comp Gs summed in kernels, ~ graph CNN without backpr
 
     G_ = []
     Et = [0,0,0,0]
-    for (_G, G) in list(combinations(N_,r=2)):  # eval comp_N -> G_
-        if _G in [G for G in G_ for G_ in G.compared__]:
-            continue  # in any rng++
+    for (_G, G) in list(combinations(N_,r=2)):
+        if _G in [G for compared_ in G.compared__ for G in compared_]:  # in any rng++
+            continue
         dy,dx = np.subtract(_G.yx,G.yx)
         dist = np.hypot(dy,dx)
         aRad = (G.aRad+_G.aRad) / 2  # ave radius to eval relative distance between G centers:
@@ -136,35 +133,43 @@ def rng_kern_(N_, rng):  # comp Gs summed in kernels, ~ graph CNN without backpr
             G.compared__[-1] += [_G]; _G.compared__[-1] += [G]
             Link = Clink(nodet=[_G,G], span=2, angle=[dy,dx], box=extend_box(G.box,_G.box))
             if comp_N(Link, Et):
+                ''' move to comp_N:
                 for g in _G,G:
                     if g not in G_: G_ += [g]
-                    if g.DerH: g.DerH.H[-1].add_(Link.derH)  # accum last DerH layer
-                    else:      g.DerH.append_(Link.derH, flat=0)  # init DerH layer with Link.derH
+                    DerH = g.extH.H[-1]
+                    if DerH: DerH.H[-1].add_(Link.derH)  # accum last DerH layer
+                    else:    DerH.append_(Link.derH, flat=0)  # init DerH layer with Link.derH
+                '''
     # + kernel rim per G:
     for G in G_:
-        G.compared__[-1] = []
+        G.compared__ += [[]]
         G.krim = [link.nodet[0] if link.nodet[1] is G else link.nodet[1] for link, rev in G.rim]
     n = 1  # n convolutions = len DerH
-    iG_ = copy(G_)  # added DerH?
+    iG_ = copy(G_)  # has added DerH
     while True:
         _G_ = []  # rng+ convolution, cross-comp: recursive center node DerH += linked node derHs for next loop:
         for G in G_:
+            H = G.derH  # comparand
+            eH = G.extH  # comp ders
             for _G in G.krim:
-                if _G in [G for G in G_ for G_ in G.compared__]:
-                    continue  # compared when _G was G
+                if _G in [G for compared_ in G.compared__ for G in compared_]:  # in any rng++ or when _G was G
+                    continue
                 G.compared__[-1] += [_G]; _G.compared__[-1] += [G]
-                dderH = _G.DerH.H[-1].comp_(G.DerH.H[-1], dderH=CH(), rn=1, fagg=1, flat=1)  # comp last krim
+                _H = _G.derH  # comparand
+                _eH = _G.derH  # comp ders
+                dderH = _H.comp_(H, dderH=CH(), rn=1, fagg=1, flat=1)  # comp last krim
                 if dderH.Et[0] > ave * dderH.Et[2] * n:  # n adds to costs
-                    for g in _G,G:  # bilateral assign
-                        g.DerH.H[n].add_(dderH) if len(g.DerH.H)>=n+1 else g.DerH.append_(dderH,flat=0)
-            #  continue rng+ / updated G:
-            if len(G.DerH.H) > n and G.DerH.H[-1].Et[0] > ave * G.DerH.H[-1].Et[2] * n:  # G.DerH may not be appended
-                _G_ += [G]  # else G kernel is not extended
+                    for eh in _eH, eH:
+                        eh.H[n].add_(dderH) if len(eh.H)>=n+1 else eh.append_(dderH,flat=0)  # bilateral assign
+            eh  = G.extH
+            if len(eh.H) > n and eh.H[-1].Et[0] > ave * eh.H[-1].Et[2] * n:  # G.extH may not be appended
+                _G_ += [G]   # else G kernel is not extended
         if _G_:
             G_ = _G_
             for _G in _G_: _G.compared__[-1] = []  # reset in intermediate rng+, nest in rng++
             n += 1
         else:
+            for G in iG_: delattr(G, "krim")
             break
 
     return iG_, Et  # Gs with added rim
@@ -456,7 +461,7 @@ def feedback(root):  # called from form_graph_, always sub+, append new der laye
     while root.fback_t[0]: mDerLay.add_(root.fback_t[0].pop())
     dDerH = CH()  # from higher-order links
     while root.fback_t[1]: dDerH.add_(root.fback_t[1].pop())
-    DerH = mDerLay.append_(dDerH, flat=1)
-    m,d, mr,dr = DerH.Et
+    DderH = mDerLay.append_(dDerH, flat=1)
+    m,d, mr,dr = DderH.Et
     if m+d > sum(G_aves) * (mr+dr):
-        root.derH.H[-1].append_(DerH, flat=0)  # append new derLay, maybe nested
+        root.derH.H[-1].append_(DderH, flat=0)  # append new derLay, maybe nested
