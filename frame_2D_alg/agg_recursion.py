0f758f7953c794fda3a1ae7982c6499d69aa3783
boris-kz <boris.kz@gmail.com>
2025-09-02 23:13:45 -0400

    edits with Chee

--- frame_2D_alg/agg_recursion.py
+++ frame_2D_alg/agg_recursion.py
@@ -278,7 +278,7 @@ def comp_Q(iN_, rc):  # comp pairs of nodes or links within max_dist
         else: break
     return N__, L_, ET
 
-def comp_N(_N,N, o,rc, L_=None, angl=np.zeros(2), span=None, fdeep=0, rng=1):  # compare links, optional angl,span,dang?
+def comp_N(_N,N, o,rc, lH=None, angl=np.zeros(2), span=None, fdeep=0, rng=1):  # compare links, optional angl,span,dang?
 
     derTT, Et, rn = min_comp(_N,N)  # -1 if _rev else 1, d = -d if L is reversed relative to _L, obsoleted by angle?
     baseT = (rn*_N.baseT+ N.baseT) /2  # not derived
@@ -292,12 +292,12 @@ def comp_N(_N,N, o,rc, L_=None, angl=np.zeros(2), span=None, fdeep=0, rng=1):  #
         if fi:
             _N_,N_ = (_N.cent_,N.cent_) if (_N.cent_ and N.cent_) else (_N.N_,N.N_)  # or both? no rim,altg_ in top nodes
             if isinstance(N_[0],CN) and isinstance(_N_[0],CN):  # not PP
-                spec(_N_,N_,o,rc,Et, Link.L_)  # use L_ for dspe?
+                spec(_N_,N_,o,rc,Et, Link.lH)  # for dspe?
     if fdeep==2: return Link  # or Et?
-    if L_ is not None:
-        L_ += [Link]
+    if lH is not None:  lH += [Link]
     for n, _n, rev in zip((N,_N),(_N,N),(0,1)):  # if rim-mediated comp: reverse dir in _N.rim: rev^_rev
-        n.rim += [Link]; n.et += Et; n.seen_.add(_n)  # or extT += Link?
+        n.rim += [Link]; n.et += Et; n.seen_.add(_n)
+        # dertt +=?
     return Link
 
 def min_comp(_N,N):  # comp Et, baseT, extT, derTT
@@ -314,8 +314,8 @@ def min_comp(_N,N):  # comp Et, baseT, extT, derTT
     m_+= md_; d_+= dd_
     DerTT = np.array([m_,d_])
     t_ = m_+ np.abs(d_) + eps
-    Et = np.array([m_* (1, mA)[fi] / t_ @ wTTf[0],
-                   d_* (1, 2-mA)[fi] / t_ @ wTTf[1], min(_n,n)])  # n: shared scope?
+    Et = np.array([m_* (1, mA)[fi] /t_ @ wTTf[0],  # norm
+                   d_* (1, 2-mA)[fi] /t_ @ wTTf[1], min(_n,n)])  # n: shared scope?
     '''
     if np.hypot(*_N.angl)*_N.mang + np.hypot(*N.angl)*N.mang > ave*wA:  # aligned L_'As, mang *= (len_nH)+fi+1
     mang = (rn*_N.mang + N.mang) / (1+rn)  # ave, weight each side by rn
@@ -323,8 +323,8 @@ def min_comp(_N,N):  # comp Et, baseT, extT, derTT
     return DerTT, Et, rn
 
 def comp_derT(_i_,i_):
-    m_ = np.minimum(np.abs(_i_), np.abs(i_))  # native vals
-    d_ = _i_ - i_  # for next comp, from signed _i_,i_
+    m_ = np.minimum(np.abs(_i_), np.abs(i_))  # native vals, probably need to be signed as before?
+    d_ = _i_-i_  # for next comp, from signed _i_,i_
     return np.array([m_,d_])
 
 def comp(_pars, pars, meA=0, deA=0):  # raw inputs or derivatives, norm to 0:1 in eval only
@@ -336,7 +336,7 @@ def comp(_pars, pars, meA=0, deA=0):  # raw inputs or derivatives, norm to 0:1 i
             m_ += [mA]; d_ += [dA]
         elif isinstance(p, tuple):  # massless I|S avd in p only
             p, avd = p
-            m_ += [avd]  # placeholder for avd / (avd+ad), no separate match
+            m_ += [avd]  # placeholder for avd / (avd+ad), no separate match, or keep signed?
             d_ += [_p - p]
         else:  # massive
             m_ += [min(abs(_p),abs(p))]
@@ -360,7 +360,7 @@ def spec(_spe,spe, o,rc, Et, dspe=None, fdeep=0):  # for N_|cent_ | altg_
         for N in spe:
             if _N is not N:
                 dN = comp_N(_N, N, o,rc, fdeep=2); Et += dN.Et  # may be d=cent_?
-                if dspe is not None: dspe += [dN]
+                if dspe is not None: dspe += [dN]  # this problem is still persists, dspe is int for links
                 if fdeep:
                     for L,l in [(L,l) for L in _N.rim for l in N.rim]:  # l nested in L
                         if L is l: Et += l.Et  # overlap val
@@ -517,34 +517,51 @@ def xcomp_C_(C_, root, rc):  # draft
     def merged(C): # get final C merge targets
         while C.fin: C = C.root
         return C
+
+    def dC_spectrum(L_):  # PCA on dC.derTT[1]), sort L_ along the principal axis of variation (PC1).
+
+        dert_ = np.array([dC.derTT[1] for dC in L_])  # arr(len(C_),dert), compute deviations from pairwise diffs
+        meanT = np.mean(dert_, axis=0)  # mean attrs
+        mad_T = np.mean( np.abs(dert_-meanT), axis=0)  # 9 MADs
+        norm_ = (dert_ - meanT) / (mad_T + eps)
+        cov_T = np.cov(norm_, rowvar=False)  # 9x9 covariance matrix
+        eig_, eig__ = np.linalg.eigh(cov_T)  # eigenvalues, eigenvectors
+        pc1_i = np.argmax(eig_)
+        pc1_T = eig__[:, pc1_i]  # eigenvector corresponding to the largest eigenvalue
+        proj_ = norm_ @ pc1_T  # sort by projection onto PC1
+        # reorder L_ to minimize ddC.derTT[1] between consecutive dCs:
+        return [L_[i] for i in np.argsort(proj_)]
+
     dC_ = []
     for _C, C in combinations(C_, r=2):
         dy_dx = _C.yx-C.yx; dist = np.hypot(*dy_dx); olp = (C.olp+_C.olp) / 2
-        dC_ += [comp_N(_C,C, olp, rc, L_=[], angl=dy_dx, span=dist)]  # add comp wTT?
+        dC_ += [comp_N(_C,C, olp, rc, nH=[], angl=dy_dx, span=dist)]
+        # add comp wTT?
     L_, lH = [], []
     dC_ = sorted(dC_, key=lambda dC: dC.Et[1])  # from min D
     for i, dC in enumerate(dC_):
         if val_(dC.Et, fi=0, aw=rc+loopw) < 0:  # merge centroids, no re-comp: merged is similar
             _C,C = dC.N_; _C,C = merged(_C), merged(C)  # final merges
-            if _C is C: continue   # already merged
-            add_N(_C,C, fmerge=1)  # set C fin,root
+            if _C is C: continue  # was merged
+            add_N(_C,C, fmerge=1)  # +fin,root
+            C_.remove(C)
         else:
             L_ = dC_[i:]  # distinct dCs
-            if L_:
-                if val_(np.sum([l.Et for l in L_], axis=0), fi=0, mw=(len(L_)-1)*Lw, aw=rc+loopw) > 0:
-                    dCt_ = []  # [(dC_,ddC_)]
-                    for _dC, dC in zip(L_,L_[1:]):  # comp consecutive dCs in D spectrum, not spatial
-                        ddC = comp_N(_dC,dC, (dC.olp+_dC.olp)/2, rc)  # += ddC in dC.rim
-                        dCt_ += [(_dC,ddC)]; _dC = dC
-                    dCt_ += [(_dC, None)]  # no ddC in last dCt
-                    dCG_ = []
-                    for (_dC,_ddC), (dC, ddC) in zip(dCt_, dCt_[1:]):
-                        if _ddC.Et[0] > ave:
-                            dC_ += [dC]  # pre-cluster
-                        else:
-                            dCG_ += [dC]; dC_ = [dC]  # term old, init new dCG; ddCs in rims
-                    dCG_ = [sum_N_(dC_) for dC_ in dCG_ + [dC_]]  # last
-                    lH = [sum_N_(dCG_)]  # single-level lH
+            if L_ and val_(np.sum([l.Et for l in L_], axis=0), fi=0, mw=(len(L_)-1)*Lw, aw=rc+loopw) > 0:
+                for C in C_:
+                    C.dertt = np.sum([l.derTT for l in C.rim]) / len(C.rim)  # ave
+                L_ = dC_spectrum(L_)  # reorder L_ to minimize ddC.derTT[1] between consecutive dCs
+                dCt_ = []
+                for _dC, dC in zip(L_,L_[1:]):  # comp consecutive dCs in D spectrum, not spatial
+                    ddC = comp_N(_dC,dC, (dC.olp+_dC.olp)/2, rc)  # += ddC in dC.rim
+                    dCt_ += [(_dC,ddC)]
+                dCt_ += [(dC,None)]  # no ddC in last dCt
+                seg_, seg = [], []
+                for (_dC,_ddC), (dC,ddC) in zip(dCt_, dCt_[1:]):
+                    if _ddC.Et[0] > ave: seg += [dC]  # pre-cluster, ddCs in rims
+                    else:                seg_ += [seg]; seg = [dC]  # term old, init new G
+                dCG_ = [sum_N_(dC_) for dC_ in seg_+[seg]]  # last
+                lH = [sum_N_(dCG_)]  # single-level lH
             break
     root.cent_ = CN(N_=list({merged(C) for C in C_}), L_=L_, lH=lH)  # add Et, + mat?
 
@@ -580,10 +597,11 @@ def sum2graph(root, node_,link_,long_,cent_, Et, olp, rng):  # sum node,link att
     graph = CN(root=root, fi=1,rng=rng, N_=node_,L_=link_,cent_=cent_,olp=olp, Et=Et, box=n0.box, baseT=n0.baseT, derTT=n0.derTT)
     graph.hL_ = long_
     n0.root = graph; yx_ = [n0.yx]; fg = fi and isinstance(n0.N_[0],CN)   # not PPs
+    # not revised:
     Nt = Copy_(n0); DerH = []  #->CN, comb forks: add_N(Nt,Nt.Lt)?
     for N in node_:
         add_H(derH,N.derH,graph); graph.baseT+=N.baseT; graph.derTT+=N.derTT; graph.box=extend_box(graph.box,N.box); yx_+=[N.yx]; N.root = graph
-        if fg: add_N(Nt,N)
+        if fg: add_N(Nt,N)  # N's root should be point to graph instead of Nt here?
     for L in link_:
         add_H(DerH,L.derH,graph); graph.baseT+=L.baseT; graph.derTT+=L.derTT
     if DerH: add_H(derH,DerH, graph)  # * rn?
@@ -612,7 +630,7 @@ def comp_H(H,h, rn, ET=None, DerTT=None, root=None):  # one-fork derH
     for _lay, lay in zip_longest(H,h):  # selective
         if _lay and lay:
             dlay = _lay.comp_lay(lay, rn, root=root)
-            derH += [dlay]; derTT = dlay.derTT; Et += dlay.Et
+            derH += [dlay]; derTT = dlay.derTT; Et[:2] += dlay.Et
     if Et[2]: DerTT += derTT; ET += Et
     return derH
 
@@ -675,7 +693,7 @@ def add_NH(H, h, root, rn=1):
 
     for Lev, lev in zip_longest(H, h, fillvalue=None):  # always aligned?
         if lev:
-            if Lev: add_N(Lev,lev)
+            if Lev: add_N(Lev,lev)  # lev.root shouldn't be updated to Lev
             else:   H += [Copy_(lev, root)]
 
 def extend_box(_box, box):
