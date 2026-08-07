8708882c02056bca8fa4fd72784697d3862aeac6
boris-kz <boris.kz@gmail.com>
2026-08-06 22:28:53 -0400

    edits with Chee

--- frame_2D_alg/meta_code.py
+++ frame_2D_alg/meta_code.py
@@ -175,7 +175,7 @@ def parse_funcs(paths):
 
 def clust_oF_():  # simplified oF rim-mediated centroid clustering
 
-    global oF_;  F_ = [F for F in oF_ if F]  # called Fs only: uncalled don't cluster, deleted at swap
+    global oF_;  F_ = [F for F in oF_ if F]  # uncalled Fs, don't cluster, pack in _F_ if the loop is not representative?
     for F in F_: F.rim = []; F.w = 0; F.root_ = set()
     for _F, F in combinations(F_,2):  # w = relative compression: shared / min cost, ave-commensurate
         if (w := comp_body(_F.body, F.body) / min(_F.fc, F.fc)) > ave:
@@ -186,7 +186,6 @@ def clust_oF_():  # simplified oF rim-mediated centroid clustering
         if F.w > ave:
             T = CoF(N_= [F]+[f for f,_ in F.rim], L_= [0 for _ in F_]); T.fin=0  # L_: dense prior w_, aligned with F_ in all Ts
             form_body(T); T_ += [T]
-        else: _F_ += [F]
     _w__ = [[0 for f in F_] for t in T_]
     fR = 1  # refine
     while fR:
@@ -206,13 +205,15 @@ def clust_oF_():  # simplified oF rim-mediated centroid clustering
                 form_body(T); fR = 1  # rebuild from remaining members, refine
             else: T.fin = 1  # converged | weak, filtered below
         _w__ = w__
-    _out_,out_ = [],[]
+    out_ = []
     for T in T_:
         if T.w > ave: out_ += [T]
-        else: out_ += T.N_
+    _F_ =[F for F in oF_ if F not in (_F for T in out_ for _F in T.N_)]  # recycle singletons
     for i,F in enumerate(list(set(out_+_F_))):
-        F.nF = i; out_ += [F]  # rename by index
+        F.nF=i; F.fdef = ast.FunctionDef(name=f'oF{F.nF}',args=ast.arguments(),body=F.body)  # reinit the fdef: ast node
+        out_ += [F]  # rename by index
     oF_ = out_
+    return oF_
 
 def comp_body(_n, n):  # compare only: compression estimate C; construction in form_body
 
